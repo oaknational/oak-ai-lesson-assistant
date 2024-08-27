3cc80a4a407c11ff6994443676a50f2335586b96
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-08-27 16:59:31 +0100

    chore: trigger build

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -4,6 +4,6 @@ import HomePage from "./home-page";
 
 export default async function Page() {
   const featureFlag = await serverSideFeatureFlag("lesson-planning-assistant");
-
+  console.log("trigger build");
   return <HomePage featureFlag={featureFlag} />;
 }
