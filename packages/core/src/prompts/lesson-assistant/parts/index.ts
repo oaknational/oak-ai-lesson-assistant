7629ac9d90549f112007c4754b5ae720a00b0f33
Stef Lewandowski <stef@stef.io>
2024-08-28 20:17:06 +0100

    feat: prompt versioning (#19)
    Refactors the Aila prompt into parts and versions the prompt as it changes over time, connecting each generation with the latest version of the prompt as snapshotted in the database

---
+++ packages/core/src/prompts/lesson-assistant/parts/index.ts
@@ -0,0 +1,12 @@
+export { americanToBritish } from "./americanToBritish";
+export { basedOn } from "./basedOn";
+export { body } from "./body";
+export { context } from "./context";
+export { endingTheInteraction } from "./endingTheInteraction";
+export { generateResponse } from "./generateResponse";
+export { interactive } from "./interactive";
+export { rag } from "./rag";
+export { schema } from "./schema";
+export { signOff } from "./signOff";
+export { task } from "./task";
+export { yourInstructions } from "./yourInstructions";
