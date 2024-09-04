a583253e2d21acf3e2e797493fbabd26eff16c26
Adam Howard <91115+codeincontext@users.noreply.github.com>
2024-09-04 13:24:01 +0200

    feat: bring nav in line with latest design

--- apps/nextjs/src/components/AppComponents/Chat/beta-tag.tsx
+++ apps/nextjs/src/components/AppComponents/Chat/beta-tag.tsx
@@ -1,6 +1,14 @@
-export default function BetaTag() {
+export function BetaTagPage() {
   return (
     <span className="flex h-[32px] items-center justify-center rounded-full bg-teachersPastelBlue px-9 py-1 text-sm font-semibold">
+      Beta
+    </span>
+  );
+}
+
+export function BetaTagHeader() {
+  return (
+    <span className="flex items-center justify-center rounded-full bg-teachersPastelBlue px-8 py-2 text-sm font-semibold text-black">
       Beta
     </span>
   );
