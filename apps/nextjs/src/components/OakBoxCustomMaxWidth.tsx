9e8bfe2fa53b4285052f4d8f613b4a1ea6524f6a
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-09-04 15:50:54 +0100

    feat: new home page copy (#54)

--- apps/nextjs/src/components/OakBoxCustomMaxWidth.tsx
+++ apps/nextjs/src/components/OakBoxCustomMaxWidth.tsx
@@ -1,6 +1,12 @@
-import { OakBox } from "@oaknational/oak-components";
+import { OakBox, OakFlex } from "@oaknational/oak-components";
 import styled from "styled-components";
 
 export const OakBoxCustomMaxWidth = styled(OakBox)<{ customMaxWidth: number }>`
+  max-width: ${(props) => props.customMaxWidth}px;
+`;
+
+export const OakFlexCustomMaxWidth = styled(OakFlex)<{
+  customMaxWidth: number;
+}>`
   max-width: ${(props) => props.customMaxWidth}px;
 `;
