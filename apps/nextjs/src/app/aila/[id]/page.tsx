dd34e370ba00d29cbffd617d5acc4ecd277bac45
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-09-04 18:08:33 +0100

    fix delete client side feature flag

--- apps/nextjs/src/app/aila/[id]/page.tsx
+++ apps/nextjs/src/app/aila/[id]/page.tsx
@@ -4,7 +4,6 @@ import { useUser } from "#clerk/nextjs";
 import { redirect } from "#next/navigation";
 
 import { trpc } from "@/utils/trpc";
-import { useClientSideFeatureFlag } from "@/utils/useClientSideFeatureFlag";
 
 import ChatPageContents from "../page-contents";
 
@@ -17,14 +16,11 @@ interface ChatPageProps {
 export default function ChatPage({ params }: Readonly<ChatPageProps>) {
   const user = useUser();
   const { id } = params;
-  const { data: chat, isLoading: isChatLoading } =
-    trpc.chat.appSessions.getChat.useQuery({ id });
-  const { data: moderations, isLoading: isModerationsLoading } =
-    trpc.chat.appSessions.getModerations.useQuery({ id });
+  const { data: chat } = trpc.chat.appSessions.getChat.useQuery({ id });
+  const { data: moderations } = trpc.chat.appSessions.getModerations.useQuery({
+    id,
+  });
 
-  const [isEnabled, isCheckComplete] = useClientSideFeatureFlag(
-    "lesson-planning-assistant",
-  );
   // For local development so that we can warm up the server
   if (id === "health") {
     return <>OK</>;
@@ -34,18 +30,9 @@ export default function ChatPage({ params }: Readonly<ChatPageProps>) {
     redirect(`/sign-in?next=/aila/${params.id}`);
   }
 
-  if (!isCheckComplete || isChatLoading || isModerationsLoading) {
-    return null;
-  }
-
   if (!chat) {
     console.log("No chat found");
     redirect("/aila?reason=no-chat-found");
-  }
-
-  if (!isEnabled) {
-    console.log("Feature flag not enabled");
-    redirect("/aila?reason=feature-flag-not-enabled");
   }
 
   return (
