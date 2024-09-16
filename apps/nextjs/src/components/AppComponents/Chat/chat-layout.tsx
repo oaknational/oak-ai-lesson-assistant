4d5ce39b14ce933ca9f8a49c5c8c4ae0b3915fe5
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-09-16 13:54:36 +0100

    feat: add in chat buttons when the chat is finished

--- apps/nextjs/src/components/AppComponents/Chat/chat-layout.tsx
+++ apps/nextjs/src/components/AppComponents/Chat/chat-layout.tsx
@@ -42,6 +42,7 @@ export const ChatLayout = ({ className }: Readonly<ChatLayoutProps>) => {
           isDemoLocked={isDemoLocked}
           showLessonMobile={showLessonMobile}
           setShowLessonMobile={setShowLessonMobile}
+          demo={demo}
         />
         <ChatRightHandSideLesson
           id={id}
--- apps/nextjs/src/components/AppComponents/Chat/chat-left-hand-side.tsx
+++ apps/nextjs/src/components/AppComponents/Chat/chat-left-hand-side.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Flex } from "@radix-ui/themes";
 import { Message } from "ai";
 
+import { DemoContextProps } from "@/components/ContextProviders/Demo";
+
 import ChatLhsHeader from "./chat-lhs-header";
 import { ChatList } from "./chat-list";
 import { ChatPanel } from "./chat-panel";
@@ -15,6 +17,7 @@ type ChatLeftHandSideProps = {
   isDemoLocked: boolean;
   showLessonMobile: boolean;
   setShowLessonMobile: (value: boolean) => void;
+  demo: DemoContextProps;
 };
 
 const ChatLeftHandSide = ({
@@ -23,6 +26,7 @@ const ChatLeftHandSide = ({
   isDemoLocked,
   showLessonMobile,
   setShowLessonMobile,
+  demo,
 }: Readonly<ChatLeftHandSideProps>) => {
   return (
     <Flex
@@ -41,6 +45,7 @@ const ChatLeftHandSide = ({
           <ChatList
             isDemoLocked={isDemoLocked}
             showLessonMobile={showLessonMobile}
+            demo={demo}
           />
         </ChatPanelArea>
         {!isDemoLocked && (
