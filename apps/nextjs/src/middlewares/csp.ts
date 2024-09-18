0da12f8c450a49f46219754e8fcdae469fab0499
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-09-18 15:32:16 +0100

    chore: csp revert

--- apps/nextjs/src/middlewares/csp.ts
+++ apps/nextjs/src/middlewares/csp.ts
@@ -4,7 +4,7 @@ const sentryEnv = process.env.NEXT_PUBLIC_SENTRY_ENV;
 const sentryRelease = process.env.NEXT_PUBLIC_APP_VERSION;
 const sentryReportUri = `${process.env.SENTRY_REPORT_URI}&sentry_environment=${sentryEnv}&sentry_release=${sentryRelease}`;
 
-const getReportUri = (): string => {
+const getReportUri = () => {
   const rate = Number.parseFloat(
     process.env.NEXT_PUBLIC_CSP_REPORT_SAMPLE_RATE || "1",
   );
@@ -14,16 +14,103 @@ const getReportUri = (): string => {
   return sentryReportUri;
 };
 
-const buildCspHeaders = (nonce: string): { policy: string } => {
+const clerkPolicies =
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ||
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "stg"
+    ? {
+        "connect-src": ["*.clerk.accounts.dev"],
+      }
+    : {};
+
+const avoPolicies =
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ||
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "stg"
+    ? {
+        // lets us use avo's debugger in dev
+        "frame-src": ["https://www.avo.app/"],
+        "connect-src": ["https://api.avo.app/"],
+      }
+    : {};
+
+const posthogPolicies =
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "dev"
+    ? {
+        "connect-src": ["https://eu.i.posthog.com"],
+      }
+    : {};
+
+const devConsentPolicies =
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "dev"
+    ? {
+        /* our consent deployment for dev doesn't live behind thenational.academy,
+        so we need to allow the specific cloud functions URL */
+        "connect-src": [
+          "https://europe-west2-oak-ai-beta-staging.cloudfunctions.net",
+        ],
+      }
+    : {};
+
+const mux = {
+  "script-src": [
+    "https://cdn.mux.com",
+    "https://mux.com",
+    "https://*.mux.com",
+    "https://stream.mux.com",
+  ],
+  "connect-src": [
+    "https://mux.com",
+    "https://*.mux.com",
+    "https://stream.mux.com",
+    "https://inferred.litix.io",
+  ],
+  "img-src": ["https://*.mux.com", "https://stream.mux.com"],
+  "style-src": ["https://*.mux.com"],
+  "media-src": [
+    "'self'",
+    "https://*.mux.com",
+    "https://stream.mux.com",
+    "blob:",
+  ],
+  "frame-src": ["https://stream.mux.com"],
+};
+
+const vercelPolicies =
+  process.env.VERCEL_ENV === "preview"
+    ? {
+        "script-src": ["https://vercel.live/", "https://vercel.com"],
+        "connect-src": [
+          "https://vercel.live/",
+          "https://vercel.com",
+          "*.pusher.com",
+          "*.pusherapp.com",
+        ],
+        "img-src": [
+          "https://vercel.live/",
+          "https://vercel.com",
+          "*.pusher.com/",
+          "data:",
+          "blob:",
+        ],
+        "frame-src": ["https://vercel.live/", "https://vercel.com"],
+        "style-src": ["https://vercel.live/"],
+        "font-src": ["https://vercel.live/", "https://assets.vercel.com"],
+      }
+    : {};
+
+const buildCspHeaders = (nonce: string) => {
+  const legacyCspHeader = `upgrade-insecure-requests; frame-ancestors 'self';script-src-next-nonce 'nonce-${nonce}'`;
+
   const baseCsp = {
     "default-src": ["'self'"],
     "media-src": ["'self'"],
     "script-src": [
       "'self'",
-      `'nonce-${nonce}'`,
+      `'nonce-${nonce}'`, // Include nonce in script-src
       "'strict-dynamic'",
       "https:",
       "http:",
+      "'unsafe-inline'", // NOTE: unsafe-inline is ignored in browsers that support nonce
+      process.env.NODE_ENV === "production" ? "" : "'unsafe-eval'",
     ],
     "style-src": ["'self'", "'unsafe-inline'"],
     "connect-src": ["'self'", "*.thenational.academy", "*.hubspot.com"],
@@ -39,7 +126,9 @@ const buildCspHeaders = (nonce: string): { policy: string } => {
     ],
     "font-src": [
       "'self'",
+      // Oak font subdomain
       "gstatic-fonts.thenational.academy",
+      // Google fonts used by third party tools
       "fonts.gstatic.com",
     ],
     "object-src": ["'none'"],
@@ -58,27 +147,63 @@ const buildCspHeaders = (nonce: string): { policy: string } => {
 
   const cspString = Object.keys(baseCsp)
     .map((policy) => {
-      const value = baseCsp[policy].join(" ");
-      return `${policy} ${value}`;
-    })
-    .join("; ");
+      const value = [...baseCsp[policy]];
 
-  return { policy: cspString };
+      if (vercelPolicies[policy]) {
+        value.push(...vercelPolicies[policy]);
+      }
+      if (clerkPolicies[policy]) {
+        value.push(...clerkPolicies[policy]);
+      }
+      if (avoPolicies[policy]) {
+        value.push(...avoPolicies[policy]);
+      }
+      if (posthogPolicies[policy]) {
+        value.push(...posthogPolicies[policy]);
+      }
+      if (devConsentPolicies[policy]) {
+        value.push(...devConsentPolicies[policy]);
+      }
+      if (mux[policy]) {
+        value.push(...mux[policy]);
+      }
+      return `${policy} ${value.join(" ")}`;
+    })
+    .join(";");
+
+  if (process.env.STRICT_CSP === "true") {
+    return {
+      policy: cspString,
+    };
+  } else {
+    return {
+      policy: legacyCspHeader,
+      reportOnly: cspString.replace("upgrade-insecure-requests ;", ""),
+    };
+  }
 };
+
+const OVERRIDE_HEADERS = "x-middleware-override-headers";
+const MIDDLEWARE_HEADER_PREFIX = "x-middleware-request" as string;
 
 const setRequestHeadersOnNextResponse = (
   res: NextResponse | Response,
-  req: NextRequest,
+  req: Request,
   newHeaders: Record<string, string>,
 ) => {
-  if (!res.headers.get("x-middleware-override-headers")) {
+  if (!res.headers.get(OVERRIDE_HEADERS)) {
+    res.headers.set(OVERRIDE_HEADERS, [...req.headers.keys()].join(","));
     req.headers.forEach((val, key) => {
-      res.headers.set(`x-middleware-request-${key}`, val);
+      res.headers.set(`${MIDDLEWARE_HEADER_PREFIX}-${key}`, val);
     });
   }
 
   Object.entries(newHeaders).forEach(([key, val]) => {
-    res.headers.set(key, val);
+    res.headers.set(
+      OVERRIDE_HEADERS,
+      `${res.headers.get(OVERRIDE_HEADERS)},${key}`,
+    );
+    res.headers.set(`${MIDDLEWARE_HEADER_PREFIX}-${key}`, val);
   });
 };
 
@@ -86,15 +211,34 @@ export const addCspHeaders = (
   response: Response,
   request: NextRequest,
 ): Response => {
+  if (
+    request.nextUrl.pathname.match(
+      /(_next\/static|_next\/image|favicon.ico)/,
+    ) ||
+    request.headers.has("next-router-prefetch") ||
+    request.headers.has("purpose")
+  ) {
+    return response;
+  }
+
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const csp = buildCspHeaders(nonce);
 
-  response.headers.set("x-middleware-csp-nonce", nonce);
-  response.headers.set("Content-Security-Policy", csp.policy);
+  const headers = new Headers(response.headers);
+  headers.set("x-middleware-csp-nonce", nonce);
 
   setRequestHeadersOnNextResponse(response, request, {
     "x-nonce": nonce,
+    "Content-Security-Policy": csp.policy,
+    ...(csp.reportOnly && {
+      "Content-Security-Policy-Report-Only": csp.reportOnly,
+    }),
   });
+
+  response.headers.set("Content-Security-Policy", csp.policy);
+  if (csp.reportOnly) {
+    response.headers.set("Content-Security-Policy-Report-Only", csp.reportOnly);
+  }
 
   return response;
 };
