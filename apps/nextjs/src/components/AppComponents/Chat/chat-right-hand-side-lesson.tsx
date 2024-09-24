3fe44cf2533d88af25bdf0bff04385c5b27de623
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-09-24 18:13:57 +0100

    chore: lint

--- apps/nextjs/src/components/AppComponents/Chat/chat-right-hand-side-lesson.tsx
+++ apps/nextjs/src/components/AppComponents/Chat/chat-right-hand-side-lesson.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 import { LooseLessonPlan } from "@oakai/aila/src/protocol/schema";
 import { OakIcon, OakSmallSecondaryButton } from "@oaknational/oak-components";
