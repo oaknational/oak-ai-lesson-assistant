e448b49bfd1d49bc66203952ea862545d8c7fb5c
MG <contact.mantagen@gmail.com>
2024-10-03 15:11:43 +0100

    chore: openai batch api helpers (#190)

---
+++ packages/ingest/src/openai-batches/writeBatchFile.ts
@@ -0,0 +1,46 @@
+import fs from "node:fs";
+
+function getBatchDataDir({ ingestId }: { ingestId: string }) {
+  return `${__dirname}/data/ingest_${ingestId}`;
+}
+
+export function writeBatchFile<T>({
+  ingestId,
+  data,
+  getBatchFileLine,
+}: {
+  ingestId: string;
+  data: T[];
+  getBatchFileLine: (datum: T) => Record<string, unknown>;
+}) {
+  return new Promise<{ filePath: string; batchDir: string }>(
+    (resolve, reject) => {
+      const timestamp = Math.floor(Date.now() / 1000);
+      const batchDir = getBatchDataDir({ ingestId });
+      fs.mkdirSync(batchDir, { recursive: true });
+      const filePath = `${batchDir}/batch_${timestamp}.jsonl`;
+      const writeStream = fs.createWriteStream(filePath, {
+        flags: "w",
+      });
+
+      writeStream.on("finish", () => {
+        resolve({ filePath, batchDir });
+        console.log("Finished writing batch file", filePath);
+      });
+
+      writeStream.on("error", reject);
+
+      for (const datum of data) {
+        try {
+          const line = getBatchFileLine(datum);
+
+          writeStream.write(`${JSON.stringify(line)}\n`);
+        } catch (error) {
+          reject(error);
+        }
+      }
+
+      writeStream.end();
+    },
+  );
+}
