d6d2368c7e6f68134bda53a2ad1fbe6095b23c49
MG <contact.mantagen@gmail.com>
2024-10-08 12:32:26 +0100

    chore: final ingest helpers (#199)

--- packages/ingest/src/IngestError.ts
+++ packages/ingest/src/IngestError.ts
@@ -1,1 +1,25 @@
-export class IngestError extends Error {}
+export class IngestError extends Error {
+  ingestId?: string;
+  lessonId?: string;
+  errorDetail?: unknown;
+
+  constructor(
+    message: string,
+    options?: ErrorOptions & {
+      ingestId?: string;
+      lessonId?: string;
+      errorDetail?: unknown;
+    },
+  ) {
+    super(message, options);
+    this.ingestId = options?.ingestId;
+    this.lessonId = options?.lessonId;
+    this.errorDetail = options?.errorDetail;
+    this.name = "IngestError";
+
+    // Ensure proper stack trace (especially in V8 environments like Node.js)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, IngestError);
+    }
+  }
+}
