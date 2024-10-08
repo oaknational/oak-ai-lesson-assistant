2e269de325e74cb79f1081551f17b5c41b121114
Stef Lewandowski <stef@stef.io>
2024-10-08 12:47:13 +0100

    Merge main

--- packages/db/prisma/migrations/20241008111057_add_key_stage_subject_index_to_snippets/migration.sql
+++ packages/db/prisma/migrations/20241008111057_add_key_stage_subject_index_to_snippets/migration.sql
@@ -1,17 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `captions_id` on the `ingest_lesson` table. All the data in the column will be lost.
-  - You are about to drop the column `lesson_plan_id` on the `ingest_lesson` table. All the data in the column will be lost.
-
-*/
--- DropIndex
-DROP INDEX "ingest"."ingest_lesson_captions_id_key";
-
--- AlterTable
-ALTER TABLE "ingest"."ingest_lesson" DROP COLUMN "captions_id",
-DROP COLUMN "lesson_plan_id";
-
 -- CreateIndex
 CREATE INDEX "lesson_summaries_key_stage_id_subject_id_idx" ON "public"."lesson_summaries"("key_stage_id", "subject_id");
 
