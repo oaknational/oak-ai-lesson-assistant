d82ca6d4a6016c945cd7e8b1236fa519c3407819
MG <contact.mantagen@gmail.com>
2024-10-16 10:46:19 +0100

    chore: allow import from csv, and teething fixes (#228)

--- packages/ingest/src/generate-lesson-plans/startGenerating.ts
+++ packages/ingest/src/generate-lesson-plans/startGenerating.ts
@@ -1,24 +1,31 @@
+import { PersistedIngest } from "../db-helpers/getIngestById";
 import { PersistedIngestLesson } from "../db-helpers/getLessonsByState";
 import { startBatch } from "../openai-batches/startBatch";
 import { OpenAiBatchSubmitCallback } from "../openai-batches/submitOpenAiBatch";
 import { CaptionsSchema } from "../zod-schema/zodSchema";
 import { getLessonPlanBatchFileLine } from "./getLessonPlanBatchFileLine";
 
-export async function startGenerating({
-  ingestId,
-  lessons,
-  onSubmitted,
-}: {
+type StartGeneratingProps = {
   ingestId: string;
   lessons: PersistedIngestLesson[];
   onSubmitted: OpenAiBatchSubmitCallback;
-}) {
+  ingest: PersistedIngest;
+};
+export async function startGenerating({
+  ingestId,
+  ingest,
+  lessons,
+  onSubmitted,
+}: StartGeneratingProps) {
   return startBatch({
     ingestId,
     data: lessons.map((lesson) => ({
       lessonId: lesson.id,
       rawLesson: lesson.data,
-      captions: CaptionsSchema.parse(lesson.captions?.data),
+      captions: lesson.captions
+        ? CaptionsSchema.parse(lesson.captions?.data)
+        : undefined,
+      ingest,
     })),
     endpoint: "/v1/chat/completions",
     getBatchFileLine: getLessonPlanBatchFileLine,
