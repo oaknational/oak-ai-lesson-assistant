28ef84086ae93304406db0950f64d387d634fc74
Stef Lewandowski <stef@stef.io>
2024-10-18 12:00:04 +0100

    Add comment to listeners

--- apps/nextjs/scripts/increase-listeners.js
+++ apps/nextjs/scripts/increase-listeners.js
@@ -1,3 +1,14 @@
+/**** 
+   Because we have a reasonably complex Next.js project now,
+   we're sometimes running into the default max listeners limit.
+   This script increases the limit to 20, which should be enough
+   so that we don't run into this issue. 
+   
+   Potentially, if we decide to move to Turbopack for compilation
+   in local development, we could remove this script. 
+   
+***/
+
 // Increase the limit of max listeners
 require("events").EventEmitter.defaultMaxListeners = 20;
 
