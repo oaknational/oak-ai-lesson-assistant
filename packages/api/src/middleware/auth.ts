1835267cddfc0ebae3df6c1f0d5373d3d7340b15
Stef Lewandowski <stef@stef.io>
2024-10-29 18:15:38 +0000

    chore: linting strings to match the new linting rules

--- packages/api/src/middleware/auth.ts
+++ packages/api/src/middleware/auth.ts
@@ -9,7 +9,7 @@ const log = aiLogger("auth");
 
 export const isLoggedInMiddleware = t.middleware(async ({ next, ctx }) => {
   if (!ctx.auth.userId) {
-    log.info({ auth: ctx.auth, url: ctx.req.url }, `User not authenticated`);
+    log.info({ auth: ctx.auth, url: ctx.req.url }, "User not authenticated");
     throw new TRPCError({
       code: "UNAUTHORIZED",
       message: "Not authenticated",
