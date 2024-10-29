e6df5a30c236df76562167ea4c29d6a64a7b0ba4
Stef Lewandowski <stef@stef.io>
2024-10-29 21:21:59 +0000

    Nullish coalescing

--- apps/nextjs/tests-e2e/config/config.ts
+++ apps/nextjs/tests-e2e/config/config.ts
@@ -7,6 +7,6 @@ export const TEST_USER_EMAIL = process.env.TEST_USER_EMAIL;
 export const TEST_USER_PASSWORD = process.env.TEST_USER_PASSWORD;
 export const PORT = process.env.PORT ?? 2525;
 export const TEST_BASE_URL =
-  process.env.TEST_BASE_URL || `http://localhost:${PORT}`;
+  process.env.TEST_BASE_URL ?? `http://localhost:${PORT}`;
 export const VERCEL_AUTOMATION_BYPASS_SECRET =
   process.env.VERCEL_AUTOMATION_BYPASS_SECRET;
