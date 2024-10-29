63b02e034762716d324f6f82134568846676529b
Stef Lewandowski <stef@stef.io>
2024-10-29 11:12:08 +0000

    fix: reintroduce custom eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["eslint:recommended", "prettier"],
-  plugins: ["turbo", "@typescript-eslint"],
+  plugins: ["turbo", "@typescript-eslint",],
   ignorePatterns: ["node_modules", "dist", "../../node_modules", "../../dist"],
   root: true,
   env: {
@@ -20,9 +20,23 @@ module.exports = {
       parser: "@typescript-eslint/parser",
       files: ["*.{ts,tsx}"],
       rules: {
-        "@typescript-eslint/quotes": ["error", "double"],
+        "no-console": "warn",
+        "@typescript-eslint/consistent-type-imports": "warn",
         "@typescript-eslint/comma-dangle": "off",
         "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/require-await": "warn",
+        "@typescript-eslint/no-unsafe-return": "warn",
+        "@typescript-eslint/no-misused-promises": "warn",
+        "@typescript-eslint/no-unsafe-argument": "warn",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-floating-promises": "warn",
+        "@typescript-eslint/unbound-method": "off",
+        "no-constant-condition": "warn",
+        "@typescript-eslint/no-unsafe-member-access": "warn",
+        "@typescript-eslint/restrict-template-expressions": "warn",
+        "@typescript-eslint/no-redundant-type-constituents": "warn",
+        "no-inner-declarations": "warn",
+        "@typescript-eslint/no-unsafe-call": "warn",
         "@typescript-eslint/explicit-function-return-type": [
           "off",
           {
@@ -44,7 +58,4 @@ module.exports = {
       },
     },
   ],
-  rules: {
-    quotes: ["error", "double"],
-  },
 };
