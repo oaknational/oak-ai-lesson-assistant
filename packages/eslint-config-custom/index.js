adc1efa686a7cc332a1755a3e77f5e50a831df7d
Stef Lewandowski <stef@stef.io>
2024-10-29 11:41:09 +0000

    fix: reintroduce custom eslint (#284)
    Co-authored-by: Adam Howard <91115+codeincontext@users.noreply.github.com>

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["eslint:recommended", "prettier"],
-  plugins: ["turbo", "@typescript-eslint"],
+  plugins: ["turbo", "@typescript-eslint",],
   ignorePatterns: ["node_modules", "dist", "../../node_modules", "../../dist"],
   root: true,
   env: {
@@ -20,9 +20,30 @@ module.exports = {
       parser: "@typescript-eslint/parser",
       files: ["*.{ts,tsx}"],
       rules: {
-        "@typescript-eslint/quotes": ["error", "double"],
+        "no-console": "warn",
+        "no-extra-boolean-cast": "warn",
+        "no-useless-escape": "warn",
+        "no-unsafe-finally": "warn",
+        "no-constant-condition": "warn",
+        "no-prototype-builtins": "warn",
+        "no-inner-declarations": "warn",
+        "@typescript-eslint/no-unsafe-enum-comparison": "warn",
+        "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+        "@typescript-eslint/consistent-type-imports": "warn",
         "@typescript-eslint/comma-dangle": "off",
         "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/require-await": "warn",
+        "@typescript-eslint/no-unsafe-return": "warn",
+        "@typescript-eslint/no-misused-promises": "warn",
+        "@typescript-eslint/no-unsafe-argument": "warn",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-floating-promises": "warn",
+        "@typescript-eslint/unbound-method": "off",
+        "@typescript-eslint/no-unsafe-member-access": "warn",
+        "@typescript-eslint/restrict-template-expressions": "warn",
+        "@typescript-eslint/no-redundant-type-constituents": "warn",        
+        "@typescript-eslint/await-thenable": "warn",
+        "@typescript-eslint/no-unsafe-call": "warn",
         "@typescript-eslint/explicit-function-return-type": [
           "off",
           {
@@ -44,7 +65,4 @@ module.exports = {
       },
     },
   ],
-  rules: {
-    quotes: ["error", "double"],
-  },
 };
