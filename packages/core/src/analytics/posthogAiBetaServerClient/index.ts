b27148426ad42085dc6506e2b84ecd0f55e1e68f
Adam Howard <91115+codeincontext@users.noreply.github.com>
2024-10-30 18:25:36 +0100

    fixes

--- packages/core/src/analytics/posthogAiBetaServerClient/index.ts
+++ packages/core/src/analytics/posthogAiBetaServerClient/index.ts
@@ -1,4 +1,5 @@
 import { PostHog } from "posthog-node";
+import invariant from "tiny-invariant";
 
 import {
   cachedFetch,
@@ -7,7 +8,8 @@ import {
 
 const host = process.env.NEXT_PUBLIC_POSTHOG_HOST as string;
 const apiKey = process.env.NEXT_PUBLIC_POSTHOG_API_KEY || "*";
-const personalApiKey = process.env.POSTHOG_PERSONAL_KEY;
+const personalApiKey = process.env.POSTHOG_PERSONAL_KEY_FLAGS;
+invariant(personalApiKey, "POSTHOG_PERSONAL_KEY_FLAGS is required");
 
 /**
  * This is the posthog nodejs client configured to send events to the
@@ -15,7 +17,6 @@ const personalApiKey = process.env.POSTHOG_PERSONAL_KEY;
  */
 export const posthogAiBetaServerClient = new PostHog(apiKey, {
   host,
-  personalApiKey,
 
   // We evaluate user feature flags on the server to prevent round-trips to posthog.
   // See https://posthog.com/docs/feature-flags/local-evaluation
@@ -23,4 +24,5 @@ export const posthogAiBetaServerClient = new PostHog(apiKey, {
   // Instead we cache them in KV through a custom fetch implementation.
   fetch: cachedFetch,
   featureFlagsPollingInterval,
+  personalApiKey,
 });
