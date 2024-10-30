33bee19e5e631428b648159452ef5af5dbec36bd
Stef Lewandowski <stef@stef.io>
2024-10-30 15:56:06 +0000

    feat: allow us to configure the server port for local testing (#308)

--- apps/nextjs/tests-e2e/config/config.ts
+++ apps/nextjs/tests-e2e/config/config.ts
@@ -5,7 +5,8 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export const TEST_USER_EMAIL = process.env.TEST_USER_EMAIL;
 export const TEST_USER_PASSWORD = process.env.TEST_USER_PASSWORD;
+export const PORT = process.env.PORT ?? 2525;
 export const TEST_BASE_URL =
-  process.env.TEST_BASE_URL || "http://localhost:2525";
+  process.env.TEST_BASE_URL ?? `http://localhost:${PORT}`;
 export const VERCEL_AUTOMATION_BYPASS_SECRET =
   process.env.VERCEL_AUTOMATION_BYPASS_SECRET;
