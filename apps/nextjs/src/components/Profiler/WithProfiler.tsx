eb9cab1019940f6874df98d52f170c64b20efcb7
Stef Lewandowski <stef@stef.io>
2024-10-31 08:59:38 +0000

    Add profiler

---
+++ apps/nextjs/src/components/Profiler/WithProfiler.tsx
@@ -0,0 +1,34 @@
+import { Profiler, useCallback } from "react";
+
+import { aiLogger } from "@oakai/logger";
+
+const isPerfEnabled = true;
+// process.env.NEXT_PUBLIC_ENABLE_PERF === "true" ||
+// process.env.NODE_ENV === "development";
+
+const log = aiLogger("profiler");
+
+export function WithProfiler({
+  id,
+  children,
+}: {
+  id: string;
+  children: React.ReactNode;
+}): React.ReactNode {
+  const handleRender = useCallback(
+    (id: string, phase: string, actualDuration: number) => {
+      if (actualDuration > 16) {
+        log.warn(`${id}: ${actualDuration}ms: ${phase} phase`);
+      }
+    },
+    [],
+  );
+
+  if (!isPerfEnabled) return children;
+
+  return (
+    <Profiler id={id} onRender={handleRender}>
+      {children}
+    </Profiler>
+  );
+}
