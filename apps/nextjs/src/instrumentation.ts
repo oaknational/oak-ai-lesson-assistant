75286b3e68cf549d8ed2234b4c96799dced1ca6d
Adam Howard <91115+codeincontext@users.noreply.github.com>
2024-10-31 14:55:07 +0100

    fix: skip instrumentation when running turbopack - fix HMR

--- apps/nextjs/src/instrumentation.ts
+++ apps/nextjs/src/instrumentation.ts
@@ -1,6 +1,12 @@
 import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
+  // https://github.com/oaknational/oak-ai-lesson-assistant/pull/328
+  // https://github.com/vercel/next.js/issues/70424
+  if (process.env.TURBOPACK) {
+    return;
+  }
+
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("../sentry.server.config");
   }
