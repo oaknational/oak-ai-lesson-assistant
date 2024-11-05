a50ff57e9cd57f53bc82080e56cb252a6818ebd0
Stef Lewandowski <stef@stef.io>
2024-11-05 11:52:17 +0000

    prd for production environment

--- apps/nextjs/src/components/AppComponents/Chat/chat-lhs-header.tsx
+++ apps/nextjs/src/components/AppComponents/Chat/chat-lhs-header.tsx
@@ -23,7 +23,7 @@ const ChatLhsHeader = ({
   return (
     <>
       <div className="mt-6 hidden items-center justify-end gap-5 sm:flex">
-        {process.env.NEXT_PUBLIC_ENVIRONMENT !== "production" && (
+        {process.env.NEXT_PUBLIC_ENVIRONMENT !== "prd" && (
           <div className="flex flex-grow flex-row space-x-4 text-left text-xs">
             <div data-testid="chat-iteration">{chat.iteration ?? 0}</div>
             <div data-testid="chat-aila-streaming-status">
