7f594a0595df432c1d56feb68dbff46fc39dbbe6
Adam Howard <91115+codeincontext@users.noreply.github.com>
2024-11-06 12:51:24 +0100

    test: set up chromatic config and get storybook building

--- apps/nextjs/.storybook/preview.tsx
+++ apps/nextjs/.storybook/preview.tsx
@@ -9,18 +9,10 @@ import "@fontsource/lexend/900.css";
 import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
 import type { Preview, Decorator } from "@storybook/react";
 
-// ModerationProvider is coming in the main Chat.tsx refactor
-//import { ModerationProvider } from "../src/components/AppComponents/Chat/Chat/ModerationProvider";
 import { TooltipProvider } from "../src/components/AppComponents/Chat/ui/tooltip";
-import { DialogProvider } from "../src/components/AppComponents/DialogContext";
-import { CookieConsentProvider } from "../src/components/ContextProviders/CookieConsentProvider";
-import { DemoProvider } from "../src/components/ContextProviders/Demo";
-import LessonPlanTrackingProvider from "../src/lib/analytics/lessonPlanTrackingContext";
-import { SidebarProvider } from "../src/lib/hooks/use-sidebar";
 import { AnalyticsProvider } from "../src/mocks/analytics/provider";
 import { TRPCReactProvider } from "../src/utils/trpc";
-import { MockClerkProvider } from "./MockClerkProvider";
-import { ThemeDecorator } from "./ThemeDecorator";
+import { RadixThemeDecorator } from "./decorators/RadixThemeDecorator";
 import "./preview.css";
 
 const preview: Preview = {
@@ -35,33 +27,29 @@ const preview: Preview = {
   tags: ["autodocs"],
 };
 
+// Providers not currently used
+// - MockClerkProvider
+// - CookieConsentProvider
+// - DemoProvider
+// - LessonPlanTrackingProvider
+// - DialogProvider
+// - OakThemeProvider
+// - SidebarProvider
+// - ChatModerationProvider
+
 export const decorators: Decorator[] = [
-  ThemeDecorator,
+  RadixThemeDecorator,
   (Story) => (
-    <MockClerkProvider>
-      <CookieConsentProvider>
-        {" "}
-        <TRPCReactProvider>
-          <DemoProvider>
-            <AnalyticsProvider>
-              <LessonPlanTrackingProvider chatId={"faked"}>
-                <DialogProvider>
-                  <OakThemeProvider theme={oakDefaultTheme}>
-                    <SidebarProvider>
-                      <TooltipProvider>
-                        {/* <ModerationProvider initialModerations={[]}> */}
-                        <Story />
-                        {/* </ModerationProvider> */}
-                      </TooltipProvider>
-                    </SidebarProvider>
-                  </OakThemeProvider>
-                </DialogProvider>
-              </LessonPlanTrackingProvider>
-            </AnalyticsProvider>
-          </DemoProvider>{" "}
-        </TRPCReactProvider>
-      </CookieConsentProvider>
-    </MockClerkProvider>
+    <>
+      {/* TODO: use a mocked provider */}
+      <TRPCReactProvider>
+        <AnalyticsProvider>
+          <TooltipProvider>
+            <Story />
+          </TooltipProvider>
+        </AnalyticsProvider>
+      </TRPCReactProvider>
+    </>
   ),
 ];
 
