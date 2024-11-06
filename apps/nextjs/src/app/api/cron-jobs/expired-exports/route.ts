dde6c14270651ff0878b62c45db545bb59a5da7d
Joe Baker <joe.baker@thenational.academy>
2024-11-06 12:10:47 +0000

    chore: sentry and logging

--- apps/nextjs/src/app/api/cron-jobs/expired-exports/route.ts
+++ apps/nextjs/src/app/api/cron-jobs/expired-exports/route.ts
@@ -1,10 +1,15 @@
 import { prisma } from "@oakai/db";
 import { googleDrive } from "@oakai/exports/src/gSuite/drive/client";
+import { aiLogger } from "@oakai/logger";
+import * as Sentry from "@sentry/node";
 import type { NextRequest } from "next/server";
+import { isTruthy } from "remeda";
+
+const log = aiLogger("cron");
 
 async function updateExpiredAt(fileIds: string[]) {
   if (fileIds.length === 0) {
-    console.log("No file IDs to update.");
+    log.info("No file IDs to update.");
     return;
   }
 
@@ -19,17 +24,17 @@ async function updateExpiredAt(fileIds: string[]) {
         expiredAt: new Date(),
       },
     });
-    console.log(`Updated expiredAt for ${fileIds.length} files.`);
+    log.info(`Updated expiredAt for ${fileIds.length} files.`);
 
     if (result.count === fileIds.length) {
-      console.log("All files updated successfully.");
+      log.info("All files updated successfully.");
     } else {
       throw new Error(
         `Expected to update ${fileIds.length} files, but only updated ${result.count}.`,
       );
     }
   } catch (error) {
-    console.error("Error updating expiredAt field in the database:", error);
+    log.error("Error updating expiredAt field in the database:", error);
     throw error;
   }
 }
@@ -38,10 +43,10 @@ async function deleteExpiredExports(fileIds: string[]) {
   try {
     for (const id of fileIds) {
       await googleDrive.files.delete({ fileId: id });
-      console.log("Deleted:", id);
+      log.info("Deleted:", id);
     }
   } catch (error) {
-    console.error("Error deleting old files from folder:", error);
+    log.error("Error deleting old files from folder:", error);
     throw error;
   }
 }
@@ -57,11 +62,11 @@ async function fetchExpiredExports({
 }: FetchExpiredExportsOptions) {
   try {
     const currentDate = new Date();
-    const oneMonthAgo = new Date(
+    const targetDate = new Date(
       currentDate.setDate(currentDate.getDate() - daysAgo),
     ).toISOString();
 
-    const query = `modifiedTime < '${oneMonthAgo}' and '${folderId}' in parents`;
+    const query = `modifiedTime < '${targetDate}' and '${folderId}' in parents`;
 
     const res = await googleDrive.files.list({
       q: query,
@@ -73,47 +78,54 @@ async function fetchExpiredExports({
       res.data.files?.filter((file) => file.ownedByMe === true) || [];
 
     if (files.length === 0) {
-      console.log(
+      log.info(
         "No files found that are older than one month in the specified folder.",
       );
       return null;
     }
 
-    console.log(`Found ${files.length} files older than one month in folder:`);
+    log.info(`Found ${files.length} files older than one month in folder:`);
     return files;
   } catch (error) {
-    console.error("Error fetching old files from folder:", error);
+    log.error("Error fetching old files from folder:", error);
     return null;
   }
 }
 
 export async function GET(request: NextRequest) {
-  const authHeader = request.headers.get("authorization");
+  try {
+    const authHeader = request.headers.get("authorization");
 
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    console.log("Authorization failed. Invalid token.");
-    return new Response("Unauthorized", { status: 401 });
+    const cronSecret = process.env.CRON_SECRET;
+    const folderId = process.env.GOOGLE_DRIVE_OUTPUT_FOLDER_ID;
+
+    if (!cronSecret) {
+      log.error("Missing cron secret");
+      return new Response("Missing cron secret", { status: 400 });
+    }
+    if (!folderId) {
+      log.error("No folder ID provided.");
+      return new Response("No folder ID provided", { status: 400 });
+    }
+
+    if (authHeader !== `Bearer ${cronSecret}`) {
+      log.error("Authorization failed. Invalid token.");
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const files = await fetchExpiredExports({ folderId, daysAgo: 14 });
+
+    if (!files || files.length === 0) {
+      return new Response("No expired files found", { status: 404 });
+    }
+
+    const validFileIds = files.map((file) => file.id).filter(isTruthy);
+
+    await updateExpiredAt(validFileIds);
+    await deleteExpiredExports(validFileIds);
+  } catch (error) {
+    Sentry.captureException(error);
   }
-
-  const folderId = process.env.GOOGLE_DRIVE_OUTPUT_FOLDER_ID;
-
-  if (!folderId) {
-    console.error("No folder ID provided.");
-    return new Response("No folder ID provided", { status: 400 });
-  }
-
-  const files = await fetchExpiredExports({ folderId, daysAgo: 14 });
-
-  if (!files || files.length === 0) {
-    return new Response("No expired files found", { status: 404 });
-  }
-
-  const validFileIds = files
-    .map((file) => file.id)
-    .filter((id): id is string => Boolean(id));
-
-  await updateExpiredAt(validFileIds);
-  await deleteExpiredExports(validFileIds);
 
   return new Response(JSON.stringify({ success: true }), { status: 200 });
 }
