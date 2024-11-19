f5e791ed6152082be340b3f8fd82561f8031d03f
Adam Howard <91115+codeincontext@users.noreply.github.com>
2024-11-19 11:51:25 +0100

    Fix missing radiz stylesheet

--- apps/nextjs/.storybook/decorators/RadixThemeDecorator.tsx
+++ apps/nextjs/.storybook/decorators/RadixThemeDecorator.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
 
 export const RadixThemeDecorator = (Story: React.ComponentType) => (
   <Theme>
