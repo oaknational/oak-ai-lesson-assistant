6b13f8680d5ad92c77c609ee005bae5391d66e3d
Joe Baker <joe.baker@thenational.academy>
2024-11-20 09:08:57 +0000

    chore: change text to sentence case - AI-237 (#355)

--- packages/aila/src/features/categorisation/categorisers/AilaCategorisation.ts
+++ packages/aila/src/features/categorisation/categorisers/AilaCategorisation.ts
@@ -56,7 +56,7 @@ ${subjects.join("\n")}
 
 LESSON TITLES
 The title of the lesson plan is the title of the lesson plan that the user wants to create. This could be anything, but it will likely be a short phrase or sentence that describes the topic of the lesson plan.
-Do not include "Lesson about" or "…Lesson" in the title. The title should be the standalone main topic of the lesson plan and not mention the word Lesson. It will be used as the title of the lesson plan in our database and displayed to the user in an overview document.
+Do not include "Lesson about" or "…Lesson" in the title. The title should be the standalone main topic of the lesson plan and not mention the word Lesson. It will be used as the title of the lesson plan in our database and displayed to the user in an overview document. The title should be in sentence case.
 
 RETURNED OBJECT
 The object you return should have the following shape:
