20136bcf487444ff03a271f05555d8077883b2f0
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-11-21 09:40:00 +0000

    chore: test

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -4,6 +4,6 @@ import HomePage from "./home-page";
 
 export default async function Page() {
   const result = await fetchAiHomepage();
-
+  // test comment
   return <HomePage pageData={result} />;
 }
