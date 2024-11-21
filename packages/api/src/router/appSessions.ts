e57035134e055224164393311960833d37a1049a
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-11-21 10:43:48 +0000

    chore: add top files

--- packages/api/src/router/appSessions.ts
+++ packages/api/src/router/appSessions.ts
@@ -199,6 +199,7 @@ export const appSessionsRouter = router({
     const sessions = await ctx.prisma.$queryRaw`
       SELECT
         id,
+        "updated_at" as "updatedAt",
         output->>'title' as title,
         output->>'isShared' as "isShared"
       FROM
@@ -224,6 +225,11 @@ export const appSessionsRouter = router({
             })
             .parse(session);
         } catch (error) {
+          Sentry.captureException(error, {
+            extra: {
+              session,
+            },
+          });
           return null;
         }
       })
