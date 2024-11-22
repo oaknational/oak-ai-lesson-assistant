cf48fdfc44e63a3acdfa280e37417f4980713a02
Adam Howard <91115+codeincontext@users.noreply.github.com>
2024-11-22 10:31:27 +0100

    test: stories for right hand side of lesson planner (#377)

---
+++ apps/nextjs/src/components/AppComponents/Chat/export-buttons/MobileExportButtons.tsx
@@ -0,0 +1,63 @@
+import { OakIcon, OakSmallSecondaryButton } from "@oaknational/oak-components";
+import Link from "next/link";
+
+import { useLessonChat } from "@/components/ContextProviders/ChatProvider";
+import { useDemoUser } from "@/components/ContextProviders/Demo";
+
+import { useDialog } from "../../DialogContext";
+
+type MobileExportButtonsProps = {
+  closeMobileLessonPullOut: () => void;
+};
+
+export const MobileExportButtons = ({
+  closeMobileLessonPullOut,
+}: MobileExportButtonsProps) => {
+  const { id } = useLessonChat();
+  const { setDialogWindow } = useDialog();
+  const demo = useDemoUser();
+
+  return (
+    <>
+      <div className="ml-[-10px] mt-27 flex justify-between px-14 pt-6 sm:hidden">
+        <button
+          onClick={() => {
+            closeMobileLessonPullOut();
+          }}
+          className={`${demo.isDemoUser ? "mt-25" : ""} flex items-center justify-center gap-3 `}
+        >
+          <span className="scale-75">
+            <OakIcon iconName="cross" />
+          </span>
+          <span className="text-base font-bold">Hide lesson</span>
+        </button>
+      </div>
+      <div className="sticky top-25 z-10 flex gap-10 bg-white p-12 sm:hidden">
+        <OakSmallSecondaryButton
+          element={Link}
+          iconName="download"
+          href={demo.isSharingEnabled ? `/aila/download/${id}` : "#"}
+          onClick={() => {
+            if (!demo.isSharingEnabled) {
+              setDialogWindow("demo-share-locked");
+            }
+          }}
+        >
+          Download
+        </OakSmallSecondaryButton>
+        <OakSmallSecondaryButton
+          iconName="share"
+          onClick={() => {
+            if (demo.isSharingEnabled) {
+              setDialogWindow("share-chat");
+            } else {
+              setDialogWindow("demo-share-locked");
+            }
+          }}
+        >
+          Share
+        </OakSmallSecondaryButton>
+      </div>
+    </>
+  );
+};
