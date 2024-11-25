d1603088e47347bfee6bc7b1dee448dc6175153e
Gareth Lomax <6199423+gclomax@users.noreply.github.com>
2024-11-25 12:13:58 +0000

    fixing imports

--- packages/aila/src/core/quiz/SimpleFullQuizService.test.ts
+++ packages/aila/src/core/quiz/SimpleFullQuizService.test.ts
@@ -2,9 +2,12 @@ import type { QuizPath } from "../../protocol/schema";
 import { AilaQuiz } from "./AilaQuiz";
 import { SimpleFullQuizService } from "./BaseFullQuizService";
 import { selectHighestRated } from "./ChoiceModels";
-import { CircleTheoremLesson } from "./CircleTheoremsExampleOutput";
-import { TestRating, testRatingSchema } from "./RerankerStructuredOutputSchema";
-import { FullQuizServiceFactory } from "./SimpleBodgeFactory";
+import { FullQuizServiceFactory } from "./FullQuizServiceFactory";
+import {
+  testRatingSchema,
+  type TestRating,
+} from "./RerankerStructuredOutputSchema";
+import { CircleTheoremLesson } from "./fixtures/CircleTheoremsExampleOutput";
 
 describe("Tests SimpleFullQuizService", () => {
   jest.setTimeout(30000);
