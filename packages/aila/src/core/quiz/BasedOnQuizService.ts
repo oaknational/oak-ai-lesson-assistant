8181229e88c62d7faa8addb7b5cc8150b47714c6
Gareth Lomax <6199423+gclomax@users.noreply.github.com>
2024-11-26 11:53:26 +0000

    changed BasedOnQuizService to use factories

--- packages/aila/src/core/quiz/BasedOnQuizService.ts
+++ packages/aila/src/core/quiz/BasedOnQuizService.ts
@@ -1,10 +1,9 @@
 import type { AilaQuizGeneratorService } from "../AilaServices";
 import { AilaQuizFactory } from "./AilaQuizGeneratorFactory";
+import { AilaQuizRerankerFactoryImpl } from "./AilaQuizRerankerFactory";
 import { BaseFullQuizService } from "./BaseFullQuizService";
 import type { BaseSchema, BaseType } from "./ChoiceModels";
-import { testRatingSchema } from "./RerankerStructuredOutputSchema";
-import { TestSchemaReranker } from "./SchemaReranker";
-import { SimpleQuizSelector } from "./SimpleQuizSelector";
+import { QuizSelectorFactoryImpl } from "./QuizSelectorFactory";
 import type { AilaQuizReranker, QuizSelector } from "./interfaces";
 
 export class BasedOnQuizService extends BaseFullQuizService {
@@ -12,8 +11,8 @@ export class BasedOnQuizService extends BaseFullQuizService {
     AilaQuizFactory.createQuizGenerator("basedOnRag"),
   ];
   public quizReranker: AilaQuizReranker<typeof BaseSchema> =
-    new TestSchemaReranker(testRatingSchema, "/starterQuiz");
+    new AilaQuizRerankerFactoryImpl().createAilaQuizReranker("schema-reranker");
 
   public quizSelector: QuizSelector<BaseType> =
-    new SimpleQuizSelector<BaseType>();
+    new QuizSelectorFactoryImpl().createQuizSelector("simple");
 }
