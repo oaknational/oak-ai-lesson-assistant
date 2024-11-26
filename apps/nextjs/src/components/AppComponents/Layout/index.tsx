69b2371bf9ee7e7f783aa191ba6932dba0171837
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-11-26 11:57:25 +0000

    feat: move delete all button and restyle side menu (#375)

--- apps/nextjs/src/components/AppComponents/Layout/index.tsx
+++ apps/nextjs/src/components/AppComponents/Layout/index.tsx
@@ -14,15 +14,17 @@ export type LayoutProps = Readonly<{
 const Layout = ({ children, includeFooter }: LayoutProps) => {
   const isDemoUser = useDemoUser().isDemoUser;
   return (
-    <div className="flex min-h-screen flex-col ">
-      <Header />
-      <main
-        className={`flex h-full flex-1 flex-col bg-muted/50 bg-white ${isDemoUser && "pt-28 sm:pt-20"}`}
-      >
-        <DialogProvider>{children}</DialogProvider>
-      </main>
-      {includeFooter && <Footer />}
-    </div>
+    <DialogProvider>
+      <div className="flex min-h-screen flex-col ">
+        <Header />
+        <main
+          className={`flex h-full flex-1 flex-col bg-muted/50 bg-white ${isDemoUser && "pt-28 sm:pt-20"}`}
+        >
+          {children}
+        </main>
+        {includeFooter && <Footer />}
+      </div>
+    </DialogProvider>
   );
 };
 
