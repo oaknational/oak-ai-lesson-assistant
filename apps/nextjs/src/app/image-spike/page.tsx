549338886f0b5e5796d5aa480cfabd5a893867f8
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2024-11-28 15:35:07 +0000

    chore: handle redirect

--- apps/nextjs/src/app/image-spike/page.tsx
+++ apps/nextjs/src/app/image-spike/page.tsx
@@ -1,23 +1,20 @@
 import { auth } from "@clerk/nextjs/server";
 import { prisma } from "@oakai/db";
+import { redirect } from "next/navigation";
 
 import ImageSpike from "./image-spike";
 
-export default async function PolicyContentPage() {
+export default async function ImageSpikePage() {
   const clerkAuthentication = auth();
   const { userId }: { userId: string | null } = clerkAuthentication;
   if (!userId) {
-    return {
-      title: "Aila",
-    };
+    redirect("/sign-in?next=/image-spike");
   }
   const lessons = await prisma.appSession.findMany({
     where: {
       userId: userId,
     },
   });
-
-  console.log(lessons.length, "***************************");
 
   return <ImageSpike lessons={lessons} />;
 }
