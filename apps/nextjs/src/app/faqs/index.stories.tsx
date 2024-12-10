f5305a7c032f41efc8deaa336eb3f43166fe4abd
Adam Howard <91115+codeincontext@users.noreply.github.com>
2024-12-10 12:05:17 +0100

    test: mobile snapshots for page stories (#436)

--- apps/nextjs/src/app/faqs/index.stories.tsx
+++ apps/nextjs/src/app/faqs/index.stories.tsx
@@ -1,10 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { FAQPageContent } from ".";
+import { chromaticParams } from "../../../.storybook/chromatic";
 
 const meta: Meta<typeof FAQPageContent> = {
   title: "Pages/FAQs",
   component: FAQPageContent,
+  parameters: {
+    ...chromaticParams(["mobile", "legacy"]),
+  },
 };
 
 export default meta;
