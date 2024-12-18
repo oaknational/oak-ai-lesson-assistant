94b2bad9201b3bf7c15501375faf0f04a3adb83d
Joe Baker <joe.baker@thenational.academy>
2024-12-18 13:14:40 +0000

    chore: add comment to skipped test

--- apps/nextjs/tests-e2e/tests/chat-performance.test.ts
+++ apps/nextjs/tests-e2e/tests/chat-performance.test.ts
@@ -27,6 +27,7 @@ test.describe("Component renders during lesson chat", () => {
     });
   });
 
+  // this is disabled because react scan is not currently working in preview deplyments.
   test.skip("There are no unnecessary rerenders across left and right side of chat", async ({
     page,
   }) => {
