9bbad07edb67b4090b2e78556f7a4a2ac7e2758f
Joe Baker <joe.baker@thenational.academy>
2024-12-19 08:43:55 +0000

    feat: react scan (#445)

--- apps/nextjs/src/app/aila/page-contents.tsx
+++ apps/nextjs/src/app/aila/page-contents.tsx
@@ -2,12 +2,17 @@
 
 import React from "react";
 
+import { useReactScan } from "hooks/useReactScan";
+
 import { Chat } from "@/components/AppComponents/Chat/Chat/chat";
+import LessonPlanDisplay from "@/components/AppComponents/Chat/chat-lessonPlanDisplay";
 import Layout from "@/components/AppComponents/Layout";
 import { ChatProvider } from "@/components/ContextProviders/ChatProvider";
 import LessonPlanTrackingProvider from "@/lib/analytics/lessonPlanTrackingContext";
 
 const ChatPageContents = ({ id }: { readonly id: string }) => {
+  useReactScan({ component: LessonPlanDisplay, interval: 10000 });
+
   return (
     <Layout>
       <LessonPlanTrackingProvider chatId={id}>
