c3fc01f8b18a9a26aa8a1cb113ed03668af46bcf
Stef Lewandowski <stef@stef.io>
2025-01-13 15:18:33 +0000

    fix: async data prep functions should use Promise.resolve (#475)

--- packages/exports/src/dataHelpers/prepWorksheetForSlides.ts
+++ packages/exports/src/dataHelpers/prepWorksheetForSlides.ts
@@ -6,7 +6,7 @@ export async function prepWorksheetForSlides(
     "title" | "cycle1" | "cycle2" | "cycle3"
   >,
 ) {
-  return {
+  return Promise.resolve({
     lesson_title: lessonPlan.title,
     learning_cycle_title_1: lessonPlan.cycle1.title ?? " ",
     practice_task_1: lessonPlan.cycle1.practice ?? " ",
@@ -14,5 +14,5 @@ export async function prepWorksheetForSlides(
     practice_task_2: lessonPlan?.cycle2?.practice ?? " ",
     learning_cycle_title_3: lessonPlan?.cycle3?.title ?? " ",
     practice_task_3: lessonPlan?.cycle3?.practice ?? " ",
-  };
+  });
 }
