8f8d36091bbeb3097445103c126faa3402655ba0
Gareth Lomax <6199423+gclomax@users.noreply.github.com>
2025-01-14 14:19:41 +0000

    improving placeholder message if quiz is not generated properly

--- packages/aila/src/utils/experimentalPatches/fetchExperimentalPatches.ts
+++ packages/aila/src/utils/experimentalPatches/fetchExperimentalPatches.ts
@@ -89,8 +89,12 @@ export async function fetchExperimentalPatches({
         mathsStarterQuiz = [
           {
             question: "No questions found",
-            answers: ["No questions found"],
-            distractors: ["No questions found"],
+            answers: [
+              "No questions found: The recommendation system you are trialling does not have suitable questions with the basedOn recommendation path. You are seeing this in place of an LLM generated quiz to make it clear that the recommendation system does not have suitable questions",
+            ],
+            distractors: [
+              "Why am i seeing this? If you do not believe you should be trialling this system please provide feedback using the flag button and selecting other. Please use this for any other feedback on the recommended quiz with the title **Experimental Quiz**",
+            ],
           },
         ];
       }
@@ -129,8 +133,12 @@ export async function fetchExperimentalPatches({
         mathsExitQuiz = [
           {
             question: "No questions found",
-            answers: ["No questions found"],
-            distractors: ["No questions found"],
+            answers: [
+              "No questions found: The recommendation system you are trialling does not have suitable questions with the basedOn recommendation path. You are seeing this in place of an LLM generated quiz to make it clear that the recommendation system does not have suitable questions",
+            ],
+            distractors: [
+              "Why am i seeing this? If you do not believe you should be trialling this system please provide feedback using the flag button and selecting other. Please use this for any other feedback on the recommended quiz with the title **Experimental Quiz**",
+            ],
           },
         ];
       }
