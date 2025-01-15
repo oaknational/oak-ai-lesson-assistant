6d8a51f5b9a70a07e7bb94f0e71745fb6036c747
Gareth Lomax <6199423+gclomax@users.noreply.github.com>
2025-01-15 10:45:37 +0000

    removing unneeded comments

--- packages/aila/src/core/quiz/rerankers/AilaQuizRerankerFactory.ts
+++ packages/aila/src/core/quiz/rerankers/AilaQuizRerankerFactory.ts
@@ -15,35 +15,7 @@ export class AilaQuizRerankerFactoryImpl implements AilaQuizRerankerFactory {
         );
       // return new TestSchemaReranker(testRatingSchema, "/starterQuiz");
       case "return-first":
-        // TODO: GCLOMAX - This needs refactoring and is an issue.
         return new ReturnFirstReranker(testRatingSchema, "/starterQuiz");
     }
   }
 }
-
-// export class AilaQuizFactoryImpl implements AilaQuizFactory {
-//   private containsMath(subject: string | undefined | null): boolean {
-//     return subject?.toLowerCase().includes("math") ?? false;
-//   }
-
-//   public quizStrategySelector(
-//     lessonPlan: LooseLessonPlan,
-//   ): quizRecommenderType {
-//     // Determine the quiz strategy based on the lesson plan
-//     const isMathsLesson = this.containsMath(lessonPlan.subject);
-//     return isMathsLesson ? "maths" : "default";
-//   }
-
-//   public createQuizRecommender(lessonPlan: LooseLessonPlan): AilaQuizService {
-//     // Determine the quiz strategy based on the lesson plan
-//     const quizType = this.quizStrategySelector(lessonPlan);
-
-//     // Create the appropriate quiz recommender
-//     switch (quizType) {
-//       case "maths":
-//         return new AilaQuiz();
-//       case "default":
-//         return new AilaQuiz();
-//     }
-//   }
-// }
