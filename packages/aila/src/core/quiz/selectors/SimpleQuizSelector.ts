bd96c3f18207d3d66ca560d5458632053ba6e070
Gareth Lomax <6199423+gclomax@users.noreply.github.com>
2025-01-15 10:43:56 +0000

    removing unneeded comments

--- packages/aila/src/core/quiz/selectors/SimpleQuizSelector.ts
+++ packages/aila/src/core/quiz/selectors/SimpleQuizSelector.ts
@@ -2,7 +2,6 @@ import type { BaseType, MaxRatingFunctionApplier } from "../ChoiceModels";
 import { selectHighestRated, type RatingFunction } from "../ChoiceModels";
 import { BaseQuizSelector } from "./BaseQuizSelector";
 
-// TODO: GCLOMAX - Why on earth is this not working?????????????
 export class SimpleQuizSelector<
   T extends BaseType,
 > extends BaseQuizSelector<T> {
