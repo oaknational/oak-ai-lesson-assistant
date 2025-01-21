3b014b359924e4093b874a6a67300cb645e60c3d
mantagen <contact.mantagen@gmail.com>
2025-01-21 10:22:44 +0000

    feat: add single lesson dry run script to ingest package

---
+++ packages/ingest/src/singleLessonDryRun.ts
@@ -0,0 +1,82 @@
+import { CompletedLessonPlanSchema } from "@oakai/aila/src/protocol/schema";
+import fs from "node:fs";
+import path from "node:path";
+import { zodResponseFormat } from "openai/helpers/zod.mjs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { getCaptionsByFileName } from "./captions/getCaptionsByFileName";
+import { getCaptionsFileNameForLesson } from "./captions/getCaptionsFileNameForLesson";
+import { getSystemPrompt } from "./generate-lesson-plans/getSystemPrompt";
+import { getUserPrompt } from "./generate-lesson-plans/getUserPrompt";
+import { graphqlClient } from "./import-lessons/graphql/client";
+import { query } from "./import-lessons/graphql/query";
+import { type RawLesson, RawLessonSchema } from "./zod-schema/zodSchema";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+void singleLessonDryRun();
+
+async function singleLessonDryRun() {
+  try {
+    const rawLesson = await fetchAndParseLesson();
+    const fileName = getCaptionsFileNameForLesson({
+      videoTitle: rawLesson.videoTitle,
+    });
+    const { caption: captions } = await getCaptionsByFileName(fileName);
+    // @todo check if transcript?
+    const systemPrompt = getSystemPrompt({ rawLesson });
+    const userPrompt = getUserPrompt({
+      rawLesson,
+      sourcePartsToInclude: "all",
+      captions,
+    });
+    const model = "gpt-4o-2024-08-06";
+    const temperature = 0.7;
+    const responseFormat = zodResponseFormat(
+      CompletedLessonPlanSchema,
+      "lesson_plan",
+    );
+
+    // Call openai completion endpoint
+    const response = {};
+    const lessonPlan = {};
+
+    const DIR = path.join(__dirname, `_data/output/${rawLesson.lessonSlug}`);
+
+    // make a folder for the lesson
+    fs.mkdirSync(DIR, { recursive: true });
+    // make a file for the lesson
+    fs.writeFileSync(
+      path.join(DIR, `lesson.json`),
+      JSON.stringify(rawLesson, null, 2),
+    );
+    // make a file for each prompt
+    fs.writeFileSync(path.join(DIR, `systemPrompt.txt`), systemPrompt);
+    fs.writeFileSync(path.join(DIR, `userPrompt.txt`), userPrompt);
+    // make a file for the lesson plan
+    fs.writeFileSync(
+      path.join(DIR, `lessonPlan.json`),
+      JSON.stringify(lessonPlan, null, 2),
+    );
+  } catch (cause) {
+    console.log(cause);
+    throw new Error("Single lesson dry run failed", { cause });
+  }
+}
+
+async function fetchAndParseLesson(): Promise<RawLesson> {
+  const lessonData = await graphqlClient.request<{ lessons: unknown[] }>(
+    query,
+    { limit: 1, offset: 0 },
+  );
+
+  const [lesson] = lessonData.lessons;
+
+  try {
+    return RawLessonSchema.parse(lesson);
+  } catch (cause) {
+    throw new Error("Failed to parse lesson", { cause });
+  }
+}
