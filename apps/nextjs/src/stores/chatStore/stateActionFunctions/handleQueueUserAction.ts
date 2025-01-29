57f7f985507dedda91eb10f30aad06c16c53bcd2
Adam Howard <91115+codeincontext@users.noreply.github.com>
2025-01-29 12:04:12 +0100

    fix warning

--- apps/nextjs/src/stores/chatStore/stateActionFunctions/handleQueueUserAction.ts
+++ apps/nextjs/src/stores/chatStore/stateActionFunctions/handleQueueUserAction.ts
@@ -2,7 +2,9 @@ import type { ChatStore } from "..";
 
 export const handleQueueUserAction =
   (set: (partial: Partial<ChatStore>) => void, get: () => ChatStore) =>
-  async (action: string) => {
+  (action: string) => {
     set({ queuedUserAction: action });
-    await get().executeQueuedAction();
+    // Typically a queued user action will be invoked later. It's not reliable to await
+    // the queued user action so we return void to discourage that use
+    void get().executeQueuedAction();
   };
