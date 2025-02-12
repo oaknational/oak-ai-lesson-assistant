225aa61a012522abcf559c993163a7a776d2b4b6
Adam Howard <91115+codeincontext@users.noreply.github.com>
2025-02-12 16:28:24 +0000

    refactor: connect lesson plan store (#532)

--- apps/nextjs/src/components/AppComponents/Chat/export-buttons/LessonPlanProgressBar.tsx
+++ apps/nextjs/src/components/AppComponents/Chat/export-buttons/LessonPlanProgressBar.tsx
@@ -1,31 +1,27 @@
 import React from "react";
 
-import { useLessonChat } from "@/components/ContextProviders/ChatProvider";
+import { useChatStore, useLessonPlanStore } from "@/stores/AilaStoresProvider";
 
 import { useProgressForDownloads } from "../Chat/hooks/useProgressForDownloads";
 
 export const LessonPlanProgressBar = () => {
-  const { lessonPlan, isStreaming } = useLessonChat();
-
+  const lessonPlan = useLessonPlanStore((state) => state.lessonPlan);
+  const isStreaming = useChatStore(
+    (state) => state.ailaStreamingStatus !== "Idle",
+  );
   const { totalSections, totalSectionsComplete } = useProgressForDownloads({
     lessonPlan,
     isStreaming,
   });
 
   return (
-    <div className="flex items-center justify-center gap-10 ">
+    <div className="flex items-center justify-center gap-10">
       <span className="flex items-center justify-center whitespace-nowrap text-base">
         {`${totalSectionsComplete} of ${totalSections} sections`}
       </span>
-      <span
-        className={`
-        relative h-8 w-full rounded-full bg-gray-200 
-        `}
-      >
+      <span className={`relative h-8 w-full rounded-full bg-gray-200`}>
         <span
-          className={`
-          absolute bottom-0 left-0 top-0 rounded-full bg-oakGreen
-          `}
+          className={`absolute bottom-0 left-0 top-0 rounded-full bg-oakGreen`}
           style={{
             width: `${(totalSectionsComplete / totalSections) * 100}%`,
           }}
