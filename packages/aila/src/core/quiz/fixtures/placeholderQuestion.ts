09dbba508edf7d63c894403dfe359efbb12b3aa7
Gareth Lomax <6199423+gclomax@users.noreply.github.com>
2025-02-12 11:37:30 +0000

    fix: typo

--- packages/aila/src/core/quiz/fixtures/placeholderQuestion.ts
+++ packages/aila/src/core/quiz/fixtures/placeholderQuestion.ts
@@ -7,7 +7,7 @@ export const placeholderQuiz: QuizQuestion[] = [
       "No questions found: The recommendation system you are trialling does not have suitable questions with the basedOn recommendation path. You are seeing this in place of an LLM generated quiz to make it clear that the recommendation system does not have suitable questions",
     ],
     distractors: [
-      "Why am i seeing this? If you do not believe you should be trialling this system please provide feedback using the flag button and selecting other. Please use this for any other feedback on the recommended quiz with the title **Experimental Quiz**",
+      "Why am I seeing this? If you do not believe you should be trialling this system please provide feedback using the flag button and selecting other. Please use this for any other feedback on the recommended quiz with the title **Experimental Quiz**",
     ],
   },
 ];
