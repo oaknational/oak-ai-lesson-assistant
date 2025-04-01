3546723e39b58c58d2277cfa8e71165f9153b48e
Joe Baker <joe.baker@thenational.academy>
2025-04-01 14:09:52 +0100

    feat: additional material package (#597)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Stef Lewandowski <stef@stef.io>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ packages/additional-materials/index.ts
@@ -0,0 +1,3 @@
+export { generateDocument } from "./src/documents/generateDocument";
+export { additionalMaterialsConfig } from "./src/documents/additionalMaterials/additionalMaterialsConfig";
+export { fetchAdditionalMaterialModeration } from "./src/moderation/fetchAdditionalMaterialModeration";
