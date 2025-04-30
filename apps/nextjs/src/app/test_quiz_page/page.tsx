178f15f61ea3a50daf98baf668ad8b769d31fb24
mantagen <contact.mantagen@gmail.com>
2025-04-30 11:45:31 +0100

    test quiz page only for Oak users

--- apps/nextjs/src/app/test_quiz_page/page.tsx
+++ apps/nextjs/src/app/test_quiz_page/page.tsx
@@ -1,10 +1,21 @@
-"use client";
-
 import { rawQuizFixture } from "@oakai/aila/src/protocol/rawQuizSchema";
+import { isOakEmail } from "@oakai/core/src/utils/isOakEmail";
+
+import { auth, clerkClient } from "@clerk/nextjs/server";
 
 import LessonOverviewQuizContainer from "@/components/AppComponents/Chat/Quiz/LessonOverviewQuizContainer";
 
-export default function TestQuizPage() {
+export default async function TestQuizPage() {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+  const user = await clerkClient.users.getUser(userId);
+  const isOakUser = isOakEmail(user.emailAddresses[0]?.emailAddress);
+  if (!isOakUser) {
+    throw new Error("User is not an Oak user");
+  }
+
   return (
     <LessonOverviewQuizContainer
       questions={rawQuizFixture}
