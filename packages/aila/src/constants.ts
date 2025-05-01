619d12e7326678b4feaee78a1e7d4e29893d475b
mantagen <contact.mantagen@gmail.com>
2025-05-01 12:09:44 +0100

    chore: update openai to 4.96

--- packages/aila/src/constants.ts
+++ packages/aila/src/constants.ts
@@ -1,10 +1,10 @@
 import type OpenAI from "openai";
 
-export const DEFAULT_MODEL: OpenAI.Chat.ChatModel = "gpt-4.1-2025-04-14";
+export const DEFAULT_MODEL: OpenAI.Chat.ChatModel = "gpt-4o-2024-08-06";
 export const DEFAULT_MODERATION_MODEL: OpenAI.Chat.ChatModel =
-  "gpt-4.1-2025-04-14";
+  "gpt-4o-2024-08-06";
 export const DEFAULT_CATEGORISE_MODEL: OpenAI.Chat.ChatModel =
-  "gpt-4.1-2025-04-14";
+  "gpt-4o-2024-08-06";
 export const DEFAULT_TEMPERATURE = 0.7;
 export const DEFAULT_MODERATION_TEMPERATURE = 0.7;
 export const DEFAULT_NUMBER_OF_RECORDS_IN_RAG = 5;
