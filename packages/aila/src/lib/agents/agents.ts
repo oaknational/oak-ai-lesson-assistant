0872b7bc56e06b6fb38989ccd594921402b1dd02
MG <contact.mantagen@gmail.com>
2025-05-21 14:35:22 +0100

    feat: quasi agentic first iteration [AI-1109] (#671)

---
+++ packages/aila/src/lib/agents/agents.ts
@@ -0,0 +1,204 @@
+import { z } from "zod";
+
+import {
+  CycleSchemaWithoutLength,
+  KeyLearningPointsSchema,
+  KeyStageSchema,
+  KeywordsSchemaWithoutLength,
+  LearningCyclesSchema,
+  LearningOutcomeSchema,
+  type LessonPlanKey,
+  LessonTitleSchema,
+  MisconceptionsSchemaWithoutLength,
+  PriorKnowledgeSchema,
+  QuizSchemaWithoutLength,
+  SubjectSchema,
+  TopicSchema,
+} from "../../protocol/schema";
+import { exitQuizInstructions } from "./prompts/exitQuizInstructions";
+import { keyLearningPointsInstructions } from "./prompts/keyLearningPointsInstructions";
+import { keywordsInstructions } from "./prompts/keywordsInstructions";
+import { learningCycleTitlesInstructions } from "./prompts/learningCycleTitlesInstructions";
+import { learningCyclesInstructions } from "./prompts/learningCyclesInstructions";
+import { learningOutcomeInstructions } from "./prompts/learningOutcomeInstructions";
+import { misconceptionsInstructions } from "./prompts/misconceptionsInstructions";
+import { priorKnowledgeInstructions } from "./prompts/priorKnowledgeInstructions";
+import { quizInstructions } from "./prompts/quizInstructions";
+import { starterQuizInstructions } from "./prompts/starterQuizInstructions";
+
+export const agentNames = z.enum([
+  "title",
+  "keyStage",
+  "subject",
+  "topic",
+  "learningOutcome",
+  "learningCycles",
+  "priorKnowledge",
+  "keyLearningPoints",
+  "misconceptions",
+  "keywords",
+  "starterQuiz",
+  "cycle",
+  "exitQuiz",
+  "mathsQuiz",
+  "deleteSection",
+  "endTurn",
+]);
+
+export type AgentName = z.infer<typeof agentNames>;
+
+const _unknownAgentResponseSchema = z.object({
+  value: z.unknown(),
+});
+export type AgentResponse = z.infer<typeof _unknownAgentResponseSchema>;
+
+const _agentResponseAnySchema = z
+  .object({
+    value: z.any(),
+  })
+  .required();
+
+type AgentResponseAny = z.infer<typeof _agentResponseAnySchema>;
+export type SchemaWithValue = z.ZodObject<{ value: z.ZodTypeAny }>;
+
+export type PromptAgentDefinition<
+  Schema extends SchemaWithValue = typeof _agentResponseAnySchema,
+> = {
+  type: "prompt";
+  name: AgentName;
+  prompt: string;
+  schema: Schema;
+  whenToUse?: string[];
+};
+export type AgentDefinition<Schema extends AgentResponse = AgentResponseAny> =
+  | PromptAgentDefinition<SchemaWithValue>
+  | {
+      type: "custom";
+      name: AgentName;
+      schema?: z.ZodTypeAny;
+      whenToUse?: string[];
+    };
+
+export const agents: Record<AgentName, AgentDefinition> = {
+  title: {
+    type: "prompt",
+    name: "title",
+    prompt: "Generate a title for the lesson plan.",
+    schema: z.object({ value: LessonTitleSchema }),
+  },
+  keyStage: {
+    type: "prompt",
+    name: "keyStage",
+    prompt: "Specify the Key Stage for this lesson.",
+    schema: z.object({ value: KeyStageSchema }),
+  },
+  subject: {
+    type: "prompt",
+    name: "subject",
+    prompt: "Specify the subject for this lesson.",
+    schema: z.object({ value: SubjectSchema }),
+  },
+  topic: {
+    type: "prompt",
+    name: "topic",
+    prompt: "Specify the topic for this lesson.",
+    schema: z.object({ value: TopicSchema }),
+  },
+  learningOutcome: {
+    type: "prompt",
+    name: "learningOutcome",
+    prompt: learningOutcomeInstructions,
+    schema: z.object({ value: LearningOutcomeSchema }),
+  },
+  learningCycles: {
+    type: "prompt",
+    name: "learningCycles",
+    prompt: learningCycleTitlesInstructions,
+    schema: z.object({ value: LearningCyclesSchema }),
+  },
+  priorKnowledge: {
+    type: "prompt",
+    name: "priorKnowledge",
+    prompt: priorKnowledgeInstructions,
+    schema: z.object({ value: PriorKnowledgeSchema }),
+  },
+  keyLearningPoints: {
+    type: "prompt",
+    name: "keyLearningPoints",
+    prompt: keyLearningPointsInstructions,
+    schema: z.object({ value: KeyLearningPointsSchema }),
+  },
+  misconceptions: {
+    type: "prompt",
+    name: "misconceptions",
+    prompt: misconceptionsInstructions,
+    schema: z.object({ value: MisconceptionsSchemaWithoutLength }),
+  },
+  keywords: {
+    type: "prompt",
+    name: "keywords",
+    prompt: keywordsInstructions,
+    schema: z.object({ value: KeywordsSchemaWithoutLength }),
+  },
+  starterQuiz: {
+    type: "prompt",
+    name: "starterQuiz",
+    prompt: [starterQuizInstructions, quizInstructions].join(`\n\n`),
+    schema: z.object({ value: QuizSchemaWithoutLength }),
+  },
+  cycle: {
+    type: "prompt",
+    name: "cycle",
+    prompt: learningCyclesInstructions,
+    schema: z.object({ value: CycleSchemaWithoutLength }),
+  },
+  exitQuiz: {
+    type: "prompt",
+    name: "exitQuiz",
+    prompt: [exitQuizInstructions, quizInstructions].join(`\n\n`),
+    schema: z.object({ value: QuizSchemaWithoutLength }),
+  },
+  mathsQuiz: {
+    type: "custom",
+    name: "mathsQuiz",
+    whenToUse: [
+      "do not use this agent if the subject is not maths/math/mathematics",
+      "only suggest this agent if the next appropriate task is the generation of a starter or exit quiz",
+      "if you select this agent, a quiz will be generated, and no other parts of the lesson will be affected",
+    ],
+  },
+  deleteSection: {
+    type: "custom",
+    name: "deleteSection",
+  },
+  endTurn: {
+    type: "custom",
+    name: "endTurn",
+  },
+};
+
+export const sectionAgentMap: Record<
+  Exclude<
+    LessonPlanKey,
+    | "title"
+    | "subject"
+    | "keyStage"
+    | "topic"
+    | "basedOn"
+    | "additionalMaterials"
+  >,
+  AgentName
+> = {
+  learningOutcome: "learningOutcome",
+  learningCycles: "learningCycles",
+  priorKnowledge: "priorKnowledge",
+  keyLearningPoints: "keyLearningPoints",
+  misconceptions: "misconceptions",
+  keywords: "keywords",
+  starterQuiz: "starterQuiz",
+  cycle1: "cycle",
+  cycle2: "cycle",
+  cycle3: "cycle",
+  exitQuiz: "exitQuiz",
+  // additionalMaterials: ["additionalMaterials"],
+};
---
+++ packages/aila/src/lib/agents/prompts/learningOutcomeInstructions.ts
@@ -0,0 +1,14 @@
+export const learningOutcomeInstructions = `LESSON LEARNING OUTCOME
+The lesson Learning Outcome describes what the pupils will have learned by the end of the lesson.
+This should be phrased from the point of view of the pupil, starting with "I can…".
+The word limit for this is 30 words and no more.
+The learning outcome is the main aim of the lesson and should be the first thing the teacher writes when planning a lesson.
+It should be clear and concise and should be the lesson's main focus.
+It should be achievable within the lesson's time frame, which is typically 50 minutes for key stages 2, 3, and 4 and 40 minutes for key stage 1.
+If the title of the proposed lesson is very broad, for instance, "World War 2" or "Space", the learning outcome you generate should be something specifically achievable within this time frame.
+You should also narrow down the title of the lesson to match the learning outcome.
+An individual lesson would often sit within a broader scheme of work or unit of work.
+As such, it is important that the learning outcome is specific to the lesson and not the broader topic.
+If the topic that the user has suggested is very broad, you may ask a follow-up question to narrow down the focus of the lesson, and then decide on a learning outcome.
+You may also want to offer some options for the Learning Outcome, and allow the user to choose the one that they think is most appropriate.
+`;
