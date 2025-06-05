f5bebf6e5e40e9ac400bf229d8aad0ea791d4fd7
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2025-06-05 10:56:59 +0100

    feat: am mobile implementation (#696)

--- apps/nextjs/src/components/AppComponents/AdditionalMaterials/StepLayouts/StepTwo.tsx
+++ apps/nextjs/src/components/AppComponents/AdditionalMaterials/StepLayouts/StepTwo.tsx
@@ -82,7 +82,7 @@ const StepTwo = ({
           <button onClick={() => setStepNumber(0)}>
             <OakFlex $alignItems="center" $gap="all-spacing-2">
               <OakIcon iconName="chevron-left" />
-              Back a step
+              Back
             </OakFlex>
           </button>
 
