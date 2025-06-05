8cc19048ceb5075e552ed914469890920884a7fd
Joe Baker <joe.baker@thenational.academy>
2025-06-05 09:35:39 +0100

    feat: moderation modal

--- apps/nextjs/src/components/DialogControl/ContentOptions/AdditionalMaterialsModeration.tsx
+++ apps/nextjs/src/components/DialogControl/ContentOptions/AdditionalMaterialsModeration.tsx
@@ -1,12 +1,7 @@
 import { OakFlex, OakP, OakPrimaryButton } from "@oaknational/oak-components";
 
-import {
-  useResourcesActions,
-  useResourcesStore,
-} from "@/stores/ResourcesStoreProvider";
+import { useResourcesStore } from "@/stores/ResourcesStoreProvider";
 import { moderationSelector } from "@/stores/resourcesStore/selectors";
-
-import ModalFooterButtons from "./ModalFooterButtons";
 
 type AdditionalMaterialsModerationProps = {
   closeDialog: () => void;
@@ -16,7 +11,7 @@ const AdditionalMaterialsModeration = ({
   closeDialog,
 }: Readonly<AdditionalMaterialsModerationProps>) => {
   const moderation = useResourcesStore(moderationSelector);
-  const { resetToDefault } = useResourcesActions();
+
   return (
     <OakFlex
       data-testid="chat-share-dialog"
@@ -24,26 +19,18 @@ const AdditionalMaterialsModeration = ({
       $height="100%"
       $flexDirection="column"
       $justifyContent="space-between"
+      $gap={"space-between-m"}
     >
-      <OakP>
-        This content that needs additional guidance. Please check carefully
-        before using. Learn more about moderation and content guidelines.
-      </OakP>
-      {moderation?.categories.map((category) => <OakP>{category}</OakP>)}
-      <OakPrimaryButton
-        onClick={() => {
-          resetToDefault();
-          closeDialog();
-        }}
-      >
-        Continue
-      </OakPrimaryButton>
-
-      <ModalFooterButtons
-        closeDialog={closeDialog}
-        // TODO: Add action button states
-        actionButtonStates={() => <OakP>Submit</OakP>}
-      />
+      <OakP>{moderation?.justification}</OakP>
+      <OakFlex $justifyContent={"center"}>
+        <OakPrimaryButton
+          onClick={() => {
+            closeDialog();
+          }}
+        >
+          Continue
+        </OakPrimaryButton>
+      </OakFlex>
     </OakFlex>
   );
 };
