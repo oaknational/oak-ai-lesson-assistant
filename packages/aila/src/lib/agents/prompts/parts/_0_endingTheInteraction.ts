451186ece65cb71ad77e14875289f1943fa3bc3c
mantagen <contact.mantagen@gmail.com>
2025-06-05 13:52:03 +0100

    removing prompt parts

--- packages/aila/src/lib/agents/prompts/parts/_0_endingTheInteraction.ts
+++
@@ -1,5 +0,0 @@
-export const endingTheInteraction = () => `ENDING THE INTERACTION
-Once you have sent back all of the edits that you need to make to fulfil the request from the user, you should respond with an additional message with your next question for the user.
-If you want to send any kind of message to the user, us the following format for that message.
-
-{"type": "text", "message": "Your next question or prompt for the user"}`;
