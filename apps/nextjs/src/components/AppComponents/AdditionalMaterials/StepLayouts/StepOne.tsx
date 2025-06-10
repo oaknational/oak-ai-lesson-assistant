3b129fb819c25bf33bd6e45e2a399d70f5c4a9b1
Tom Wise <79859203+tomwisecodes@users.noreply.github.com>
2025-06-10 11:59:03 +0100

    chore: add button to step 1

--- apps/nextjs/src/components/AppComponents/AdditionalMaterials/StepLayouts/StepOne.tsx
+++ apps/nextjs/src/components/AppComponents/AdditionalMaterials/StepLayouts/StepOne.tsx
@@ -82,7 +82,14 @@ const StepOne = () => {
       </OakFlex>
 
       <ResourcesFooter>
-        <OakFlex $justifyContent="flex-end" $width={"100%"}>
+        <OakFlex $justifyContent="space-between" $width={"100%"}>
+          <button onClick={() => setStepNumber(0)}>
+            {/* Todo: Link this up to the previous step when for launch */}
+            <OakFlex $alignItems="center" $gap="all-spacing-2">
+              <OakIcon iconName="chevron-left" />
+              Back
+            </OakFlex>
+          </button>
           <OakPrimaryButton
             onClick={() => setStepNumber(1)}
             iconName="arrow-right"
