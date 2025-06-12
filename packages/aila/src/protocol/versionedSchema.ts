bac64e2b5ac6a0dff594fe17297359b5cca0f13c
mantagen <contact.mantagen@gmail.com>
2025-06-12 14:44:28 +0100

    simplify

--- packages/aila/src/protocol/versionedSchema.ts
+++ packages/aila/src/protocol/versionedSchema.ts
@@ -3,24 +3,20 @@ import { z } from "zod";
 export const MultipleChoiceQuestionSchema = z.object({
   questionType: z.literal("multiple-choice"),
   question: z.string(),
-  answers: z.array(
-    z.object({
-      text: z.string(),
-      isCorrect: z.boolean(),
-    }),
-  ),
+  answers: z.array(z.string()),
+  distractors: z.array(z.string()),
 });
 
 export const MatchQuestionSchema = z.object({
   questionType: z.literal("match"),
   question: z.string(),
-  answers: z.array(z.tuple([z.string(), z.string()])),
+  pairs: z.array(z.tuple([z.string(), z.string()])),
 });
 
 export const OrderQuestionSchema = z.object({
   questionType: z.literal("order"),
   question: z.string(),
-  answers: z.array(
+  items: z.array(
     z.object({
       text: z.string(),
       correctPosition: z.number(),
