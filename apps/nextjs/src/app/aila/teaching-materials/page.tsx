87110882cd83187af1a56d1276714239ad8f6f4c
Joe Baker <joe.baker@thenational.academy>
2025-06-13 14:27:12 +0100

    chore: remove feature flag

--- apps/nextjs/src/app/aila/teaching-materials/page.tsx
+++ apps/nextjs/src/app/aila/teaching-materials/page.tsx
@@ -1,15 +1,5 @@
-import { redirect } from "next/navigation";
-
-import { serverSideFeatureFlag } from "@/utils/serverSideFeatureFlag";
-
 import ResourcesContents from "./resources-contents";
 
-export default async function AdditionalMaterialsTestPage() {
-  const canSeeAM = await serverSideFeatureFlag("additional-materials");
-
-  if (!canSeeAM) {
-    redirect("/");
-  }
-
+export default function AdditionalMaterialsTestPage() {
   return <ResourcesContents />;
 }
