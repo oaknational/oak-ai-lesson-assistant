02af2f54987b13433247af3c9fbfb5d569085824
mantagen <contact.mantagen@gmail.com>
2025-08-14 17:15:14 +0100

    update to gpt-5

--- packages/aila/src/constants.ts
+++ packages/aila/src/constants.ts
@@ -1,10 +1,9 @@
 import type OpenAI from "openai";
 
-export const DEFAULT_MODEL: OpenAI.Chat.ChatModel = "gpt-4.1-2025-04-14";
+export const DEFAULT_MODEL: OpenAI.Chat.ChatModel = "gpt-5";
 export const DEFAULT_MODERATION_MODEL: OpenAI.Chat.ChatModel =
   "gpt-4.1-2025-04-14";
-export const DEFAULT_CATEGORISE_MODEL: OpenAI.Chat.ChatModel =
-  "gpt-4.1-2025-04-14";
+export const DEFAULT_CATEGORISE_MODEL: OpenAI.Chat.ChatModel = "gpt-5";
 export const DEFAULT_TEMPERATURE = 0.7;
 export const DEFAULT_MODERATION_TEMPERATURE = 0.7;
 export const DEFAULT_NUMBER_OF_RECORDS_IN_RAG = 5;
