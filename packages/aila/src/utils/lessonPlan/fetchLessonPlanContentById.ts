e85b40dd9e60e4cfeba2df9e83beffe9df7a46a0
Adam Howard <91115+codeincontext@users.noreply.github.com>
2025-08-28 16:33:36 +0200

    bug: upgrade quizzes to v2 in rag lessons

--- packages/aila/src/utils/lessonPlan/fetchLessonPlanContentById.ts
+++ packages/aila/src/utils/lessonPlan/fetchLessonPlanContentById.ts
@@ -1,8 +1,22 @@
 import type { PrismaClientWithAccelerate } from "@oakai/db";
 
+import invariant from "tiny-invariant";
+
 import { tryWithErrorReporting } from "../../helpers/errorReporting";
 import type { LooseLessonPlan } from "../../protocol/schema";
 import { LessonPlanSchemaWhilstStreaming } from "../../protocol/schema";
+import { upgradeQuizzes } from "../../protocol/schemas/quiz/conversion/lessonPlanQuizMigrator";
+
+// asserts that the object has a lessonPlan property
+function hasLessonPlanKey(obj: unknown): obj is { lessonPlan: unknown } {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "lessonPlan" in obj &&
+    typeof obj.lessonPlan === "object" &&
+    obj.lessonPlan !== null
+  );
+}
 
 export async function fetchLessonPlanContentById(
   id: string,
@@ -18,8 +32,23 @@ export async function fetchLessonPlanContentById(
   if (!lessonPlanRecord) {
     return null;
   }
+
+  const upgradeResult = await upgradeQuizzes({
+    data: {
+      lessonPlan: lessonPlanRecord.content,
+    },
+    persistUpgrade: null,
+  });
+
+  // upgradeQuizzes expects and returns an object with a lessonPlan property
+  invariant(
+    hasLessonPlanKey(upgradeResult.data),
+    "Failed to upgrade lesson plan content",
+  );
+  const upgradedLessonPlan = upgradeResult.data.lessonPlan;
+
   const parsedPlan = tryWithErrorReporting(
-    () => LessonPlanSchemaWhilstStreaming.parse(lessonPlanRecord.content),
+    () => LessonPlanSchemaWhilstStreaming.parse(upgradedLessonPlan),
     "Failed to parse lesson plan content",
     undefined,
     undefined,
