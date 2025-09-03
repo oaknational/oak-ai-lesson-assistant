679660eaed0413c22bd78a8f865044cec9fa299f
mantagen <contact.mantagen@gmail.com>
2025-09-03 16:13:31 +0100

    fixgit add -p

--- packages/aila/src/constants.ts
+++ packages/aila/src/constants.ts
@@ -2,9 +2,9 @@ import type OpenAI from "openai";
 
 import type { QuizGeneratorType } from "./core/quiz/schema";
 
-const DEFAULT_MODEL: OpenAI.Chat.ChatModel = "gpt-4o-2024-08-06";
+const DEFAULT_MODEL: OpenAI.Chat.ChatModel = "gpt-5-mini-2025-08-07";
 const DEFAULT_MODERATION_MODEL: OpenAI.Chat.ChatModel = "gpt-4.1-2025-04-14";
-const DEFAULT_CATEGORISE_MODEL: OpenAI.Chat.ChatModel = "gpt-5";
+const DEFAULT_CATEGORISE_MODEL: OpenAI.Chat.ChatModel = "gpt-5-mini-2025-08-07";
 const DEFAULT_TEMPERATURE = 0.7;
 const DEFAULT_MODERATION_TEMPERATURE = 0.7;
 export const DEFAULT_NUMBER_OF_RECORDS_IN_RAG = 5;
@@ -15,15 +15,25 @@ export const DEFAULT_QUIZ_GENERATORS: QuizGeneratorType[] = [
 ];
 export const BOT_USER_ID = "bot";
 
-type GPT4Params = {
+type Params = {
   model: OpenAI.Chat.ChatModel;
+};
+
+type GPT4Params = {
+  model: Extract<
+    OpenAI.Chat.ChatModel,
+    "gpt-4o-2024-08-06" | "gpt-4.1-2025-04-14" | "sef"
+  >;
   temperature: number;
 };
 
 type Verbosity = "low" | "medium" | "high";
 type ReasoningEffort = "minimal" | "low" | "medium" | "high";
 type GPT5Params = {
-  model: "gpt-5-2025-08-07" | "gpt-5-mini-2025-08-07";
+  model: Extract<
+    OpenAI.Chat.ChatModel,
+    "gpt-5-2025-08-07" | "gpt-5-mini-2025-08-07"
+  >;
   reasoning_effort: ReasoningEffort;
   verbosity: Verbosity;
 };
@@ -31,12 +41,12 @@ type GPT5Params = {
 export type OpenAIModelParams = GPT4Params | GPT5Params;
 
 export const DEFAULT_OPENAI_GPT4_PARAMS: GPT4Params = {
-  model: DEFAULT_MODEL,
+  model: "gpt-4o-2024-08-06",
   temperature: DEFAULT_TEMPERATURE,
 };
 
 export const DEFAULT_OPENAI_GPT5_PARAMS: GPT5Params = {
-  model: "gpt-5-mini-2025-08-07",
+  model: DEFAULT_MODEL,
   reasoning_effort: "low",
   verbosity: "low",
 };
@@ -47,18 +57,18 @@ export const DEFAULT_MODERATION_GPT4_PARAMS: GPT4Params = {
 };
 
 export const DEFAULT_MODERATION_GPT5_PARAMS: GPT5Params = {
-  model: "gpt-5-mini-2025-08-07",
+  model: DEFAULT_MODEL,
   reasoning_effort: "low",
   verbosity: "low",
 };
 
 export const DEFAULT_CATEGORISE_GPT4_PARAMS: GPT4Params = {
-  model: DEFAULT_CATEGORISE_MODEL,
+  model: "gpt-4o-2024-08-06",
   temperature: DEFAULT_TEMPERATURE,
 };
 
 export const DEFAULT_CATEGORISE_GPT5_PARAMS: GPT5Params = {
-  model: "gpt-5-mini-2025-08-07",
+  model: DEFAULT_MODEL,
   reasoning_effort: "low",
   verbosity: "low",
 };
