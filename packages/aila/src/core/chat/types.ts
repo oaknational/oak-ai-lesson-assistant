2c38bf61c8645a5fdcc5bd30e615bf538c95a9ec
mantagen <contact.mantagen@gmail.com>
2025-09-24 12:49:17 +0100

    message schema fix

--- packages/aila/src/core/chat/types.ts
+++ packages/aila/src/core/chat/types.ts
@@ -4,12 +4,12 @@ export const MessageSchema = z.object({
   id: z.string(),
   content: z.string(),
   role: z.union([
-    z.literal("function"),
+    // z.literal("function"),
     z.literal("data"),
     z.literal("user"),
     z.literal("system"),
     z.literal("assistant"),
-    z.literal("tool"),
+    // z.literal("tool"),
   ]),
 });
 
