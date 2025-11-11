ebc5ef5eae7b1c1acd47976c9168a36d9c47af97
Joe Baker <joe.baker@thenational.academy>
2025-11-11 09:22:13 +0000

    chore: logs for slack message

--- packages/core/src/functions/slack/notifyThreatDetectionAila.ts
+++ packages/core/src/functions/slack/notifyThreatDetectionAila.ts
@@ -1,3 +1,5 @@
+import { aiLogger } from "@oakai/logger";
+
 import { inngest } from "../../inngest";
 import {
   actionsBlock,
@@ -11,6 +13,8 @@ import {
 } from "../../utils/slack";
 import { notifyThreatDetectionAilaSchema } from "./notifyThreatDetectionAila.schema";
 
+const log = aiLogger("inngest");
+
 export const notifyThreatDetectionAila = inngest.createFunction(
   {
     name: "Notify in slack when a threat is detected in Aila chat",
@@ -19,35 +23,61 @@ export const notifyThreatDetectionAila = inngest.createFunction(
   { event: "app/slack.notifyThreatDetectionAila" },
   async ({ event, step }) => {
     await step.run("Send message to slack", async () => {
-      const args = notifyThreatDetectionAilaSchema.data.parse(event.data);
+      try {
+        log.info("Attempting to send Aila threat detection to Slack", {
+          userId: event.user.id,
+          chatId: event.data.chatId,
+        });
 
-      const getSlackThreadDetectionsData = formatThreatDetectionWithMessages(
-        args.threatDetection,
-        event.data.messages,
-      );
+        const args = notifyThreatDetectionAilaSchema.data.parse(event.data);
 
-      const response = await slackWebClient.chat.postMessage({
-        channel: slackNotificationChannelId,
-        text: "Threat detected - Aila",
-        blocks: [
-          createHeaderBlock("Threat detected - Aila"),
-          userIdBlock(event.user.id),
-          chatLinkBlock(args.chatId),
-          createThreatSectionBlock({
-            id: args.chatId,
-            userInput: getSlackThreadDetectionsData.userInput,
-            detectedThreats: getSlackThreadDetectionsData.detectedThreats,
-            requestId: getSlackThreadDetectionsData.requestId,
-            userAction: args.userAction,
-          }),
-          actionsBlock({
-            userActionsProps: { userId: event.user.id },
-            chatActionsProps: { chatId: args.chatId },
-          }),
-        ],
-      });
+        const getSlackThreadDetectionsData = formatThreatDetectionWithMessages(
+          args.threatDetection,
+          event.data.messages,
+        );
 
-      return response;
+        const response = await slackWebClient.chat.postMessage({
+          channel: slackNotificationChannelId,
+          text: "Threat detected - Aila",
+          blocks: [
+            createHeaderBlock("Threat detected - Aila"),
+            userIdBlock(event.user.id),
+            chatLinkBlock(args.chatId),
+            createThreatSectionBlock({
+              id: args.chatId,
+              userInput: getSlackThreadDetectionsData.userInput,
+              detectedThreats: getSlackThreadDetectionsData.detectedThreats,
+              requestId: getSlackThreadDetectionsData.requestId,
+              userAction: args.userAction,
+            }),
+            actionsBlock({
+              userActionsProps: { userId: event.user.id },
+              chatActionsProps: { chatId: args.chatId },
+            }),
+          ],
+        });
+
+        log.info("Successfully sent Aila threat detection to Slack", {
+          userId: event.user.id,
+          chatId: args.chatId,
+          messageTs: response.ts,
+        });
+
+        return response;
+      } catch (error) {
+        log.error(
+          "Failed to send Aila threat detection to Slack",
+          {
+            userId: event.user.id,
+            chatId: event.data.chatId,
+            error: error instanceof Error ? error.message : String(error),
+          },
+          error,
+        );
+        throw new Error("Failed to send Aila threat detection to Slack", {
+          cause: error,
+        });
+      }
     });
   },
 );
