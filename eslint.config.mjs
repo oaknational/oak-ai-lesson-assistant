ac1d85668415854ec7eef914e39389b8aacf3e27
Adam Howard <91115+codeincontext@users.noreply.github.com>
2025-11-21 13:17:46 +0100

    fix: fix vercelkv version issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import config from "@oakai/eslint-config";
-
-import storybook from "eslint-plugin-storybook";
 
 export default config;
