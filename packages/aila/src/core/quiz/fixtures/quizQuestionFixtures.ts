a7c77a9b50733dad13f36be6c89ccd5f96bef857
Adam Howard <91115+codeincontext@users.noreply.github.com>
2025-11-24 22:41:33 +0100

    refactor(quiz): rename CachedImageQuiz to quizQuestionFixtures
    Rename fixture file to better reflect its purpose as test fixtures
    rather than implying caching functionality.

--- packages/aila/src/core/quiz/fixtures/quizQuestionFixtures.ts
+++ packages/aila/src/core/quiz/fixtures/quizQuestionFixtures.ts
@@ -1,15 +1,15 @@
 /* cspell:disable */
-import type { PatchDocument } from "../../../protocol/jsonPatchProtocol";
-import type {
-  QuizQuestionPool,
-  QuizQuestionWithSourceData,
-} from "../interfaces";
+import type { QuizQuestionPool, RagQuizQuestion } from "../interfaces";
 
-export const cachedQuiz: QuizQuestionWithSourceData[] = [
+export const cachedQuiz: RagQuizQuestion[] = [
   {
-    question: "How many degrees in 2 right angles?",
-    answers: ["180°"],
-    distractors: ["60°", "90°"],
+    question: {
+      questionType: "short-answer",
+      question:
+        "Adjacent multiples of 8 can be found by increasing or decreasing a multiple by {{ }}.",
+      answers: ["8", "eight"],
+      hint: "Think about the words increase and decrease. You could think of adding and subtracting.",
+    },
     sourceUid: "QUES-BPWF2-29205",
     source: {
       questionId: 229205,
@@ -47,15 +47,16 @@ export const cachedQuiz: QuizQuestionWithSourceData[] = [
       hint: "Think about the words increase and decrease. You could think of adding and subtracting.",
       active: false,
     },
+    imageMetadata: [],
   },
   {
-    question:
-      "Two shapes are {{}} if the only difference between them is their size.",
-    answers: ["similar"],
-    distractors: [
-      "No distractors for short answer",
-      "No distractors for short answer",
-    ],
+    question: {
+      questionType: "short-answer",
+      question:
+        "Two shapes are {{}} if the only difference between them is their size.",
+      answers: ["similar"],
+      hint: "Think about shapes that look the same but are different sizes.",
+    },
     sourceUid: "QUES-RLYY2-29206",
     source: {
       questionId: 229206,
@@ -85,15 +86,16 @@ export const cachedQuiz: QuizQuestionWithSourceData[] = [
       hint: "Think about shapes that look the same but are different sizes.",
       active: true,
     },
+    imageMetadata: [],
   },
   {
-    question:
-      "A fruit stall is having a sale. It sells cherries in boxes of four pairs. How many cherries are there in six packs? There will be {{ }} in six packs.",
-    answers: ["24"],
-    distractors: [
-      "No distractors for short answer",
-      "No distractors for short answer",
-    ],
+    question: {
+      questionType: "short-answer",
+      question:
+        "A fruit stall is having a sale. It sells cherries in boxes of four pairs. How many cherries are there in six packs? There will be {{ }} in six packs.",
+      answers: ["24"],
+      hint: "Each box contains 4 pairs of cherries. How many cherries is that per box?",
+    },
     sourceUid: "QUES-XRES2-29207",
     source: {
       questionId: 229207,
@@ -123,18 +125,24 @@ export const cachedQuiz: QuizQuestionWithSourceData[] = [
       hint: "Each box contains 4 pairs of cherries. How many cherries is that per box?",
       active: true,
     },
+    imageMetadata: [],
   },
   {
-    question:
-      "In which image is the circumference labelled with a question mark?",
-    answers: [
-      "![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1703169784/fg4uyx41rfnksbvav2nh.png)",
-    ],
-    distractors: [
-      "![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1703163380/pz6cn5k4wmowycnjq5am.png)",
-      "![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1703169784/mr09mrwkqdtk1dvjdoi0.png)",
-    ],
+    question: {
+      questionType: "multiple-choice",
+      question:
+        "In which image is the circumference labelled with a question mark?",
+      answers: [
+        "![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1703169784/fg4uyx41rfnksbvav2nh.png)",
+      ],
+      distractors: [
+        "![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1703163380/pz6cn5k4wmowycnjq5am.png)",
+        "![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1703169784/mr09mrwkqdtk1dvjdoi0.png)",
+      ],
+      hint: "Look for the image where the circumference (the distance around the circle) is marked with a question mark.",
+    },
     sourceUid: "QUES-RKQC2-29208",
+    imageMetadata: [],
     source: {
       questionId: 229208,
       questionUid: "QUES-RKQC2-29208",
@@ -183,14 +191,19 @@ export const cachedQuiz: QuizQuestionWithSourceData[] = [
     },
   },
   {
-    question:
-      "Complete the statement. Triangle ABC and triangle XYZ are ____________. ![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1706110974/fukcqeavzcevgjhmm1n4.png)",
-    answers: ["similar as the three interior angles are the same."],
-    distractors: [
-      "congruent as the three interior angles are all the same.",
-      "neither similar nor congruent.",
-    ],
+    question: {
+      questionType: "multiple-choice",
+      question:
+        "Complete the statement. Triangle ABC and triangle XYZ are ____________. ![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1706110974/fukcqeavzcevgjhmm1n4.png)",
+      answers: ["similar as the three interior angles are the same."],
+      distractors: [
+        "congruent as the three interior angles are all the same.",
+        "neither similar nor congruent.",
+      ],
+      hint: "Look at the angles in both triangles. Are they the same?",
+    },
     sourceUid: "QUES-FZZN2-29209",
+    imageMetadata: [],
     source: {
       questionId: 229209,
       questionUid: "QUES-FZZN2-29209",
@@ -250,12 +263,16 @@ export const cachedQuiz: QuizQuestionWithSourceData[] = [
   },
 ];
 
-export const cachedBadQuiz: QuizQuestionWithSourceData[] = [
+export const cachedBadQuiz: RagQuizQuestion[] = [
   {
-    question: "angles?????",
-    answers: ["180"],
-    distractors: ["elephant", "banana"],
+    question: {
+      questionType: "short-answer",
+      question: "angles?????",
+      answers: ["180"],
+      hint: "just guess lol",
+    },
     sourceUid: "QUES-BAD1-999001",
+    imageMetadata: [],
     source: {
       questionId: 999001,
       questionUid: "QUES-BAD1-999001",
@@ -285,11 +302,15 @@ export const cachedBadQuiz: QuizQuestionWithSourceData[] = [
     },
   },
   {
-    question:
-      "SHAPES ARE WHAT WHEN THEY LOOK KINDA THE SAME BUT DIFFERENT SIZES????????",
-    answers: ["similar", "Similar", "SIMILAR", "similar"],
-    distractors: ["idk", "maybe"],
+    question: {
+      questionType: "short-answer",
+      question:
+        "SHAPES ARE WHAT WHEN THEY LOOK KINDA THE SAME BUT DIFFERENT SIZES????????",
+      answers: ["similar", "Similar", "SIMILAR"],
+      hint: "just type similar in any case",
+    },
     sourceUid: "QUES-BAD2-999002",
+    imageMetadata: [],
     source: {
       questionId: 999002,
       questionUid: "QUES-BAD2-999002",
@@ -337,11 +358,15 @@ export const cachedBadQuiz: QuizQuestionWithSourceData[] = [
     },
   },
   {
-    question:
-      "cherries cost £5 per box and boxes have 8 cherries but then there's a sale for 20% off and you buy 3 boxes how many cherries????",
-    answers: ["24"],
-    distractors: ["£12", "sale price"],
+    question: {
+      questionType: "short-answer",
+      question:
+        "cherries cost £5 per box and boxes have 8 cherries but then there's a sale for 20% off and you buy 3 boxes how many cherries????",
+      answers: ["24"],
+      hint: "just put 24 trust me",
+    },
     sourceUid: "QUES-BAD3-999003",
+    imageMetadata: [],
     source: {
       questionId: 999003,
       questionUid: "QUES-BAD3-999003",
@@ -371,12 +396,17 @@ export const cachedBadQuiz: QuizQuestionWithSourceData[] = [
     },
   },
   {
-    question: "which one has the ? on it lol",
-    answers: [
-      "![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1703169784/fg4uyx41rfnksbvav2nh.png)",
-    ],
-    distractors: ["this one", "the other one"],
+    question: {
+      questionType: "multiple-choice",
+      question: "which one has the ? on it lol",
+      answers: [
+        "![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1703169784/fg4uyx41rfnksbvav2nh.png)",
+      ],
+      distractors: ["this one", "the other one"],
+      hint: "look for the ? mark",
+    },
     sourceUid: "QUES-BAD4-999004",
+    imageMetadata: [],
     source: {
       questionId: 999004,
       questionUid: "QUES-BAD4-999004",
@@ -424,11 +454,16 @@ export const cachedBadQuiz: QuizQuestionWithSourceData[] = [
     },
   },
   {
-    question:
-      "triangles ![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1706110974/fukcqeavzcevgjhmm1n4.png)",
-    answers: ["theyre the same angles init"],
-    distractors: ["they look pretty", "triangles are my favourite shape"],
+    question: {
+      questionType: "multiple-choice",
+      question:
+        "triangles ![image](http://oaknationalacademy-res.cloudinary.com/image/upload/v1706110974/fukcqeavzcevgjhmm1n4.png)",
+      answers: ["theyre the same angles init"],
+      distractors: ["they look pretty", "triangles are my favourite shape"],
+      hint: "the angles are the same innit",
+    },
     sourceUid: "QUES-BAD5-999005",
+    imageMetadata: [],
     source: {
       questionId: 999005,
       questionUid: "QUES-BAD5-999005",
@@ -488,7 +523,7 @@ export const cachedBadQuiz: QuizQuestionWithSourceData[] = [
 ];
 
 export function createMockQuestionPool(
-  questions: QuizQuestionWithSourceData[],
+  questions: RagQuizQuestion[],
   id: string = "test-id",
   title: string = "Test Lesson",
 ): QuizQuestionPool {
