8012400cabcdb1c7612caf11dca1034c26f87321
mantagen <contact.mantagen@gmail.com>
2026-03-09 17:41:04 +0000

    fix: reduce sentry trace sampling to 0.05

--- apps/nextjs/sentry.server.config.ts
+++ apps/nextjs/sentry.server.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
   release: process.env.NEXT_PUBLIC_APP_VERSION,
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.05,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
