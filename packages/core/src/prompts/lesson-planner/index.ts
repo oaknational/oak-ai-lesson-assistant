4ad2b520e98f36d5aeeb91a46a8f44294322c63c
Adam Howard <91115+codeincontext@users.noreply.github.com>
2026-03-16 13:21:24 +0100

    chore: remove setupPrompts script and dead lesson-planner prompts (#985)

--- packages/core/src/prompts/lesson-planner/index.ts
+++
@@ -1,9 +0,0 @@
-import { extendLessonPlanQuiz } from "./extend-lesson-plan-quiz";
-import { generateLessonPlan } from "./generate-lesson-plan";
-import { regenerateLessonPlan } from "./regenerate-lesson-plan";
-
-export default {
-  generateLessonPlan,
-  regenerateLessonPlan,
-  extendLessonPlanQuiz,
-};
