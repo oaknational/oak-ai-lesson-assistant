953d0655bdbf82c6add373c0c00e1fd7e29e76a3
Adam Howard <91115+codeincontext@users.noreply.github.com>
2026-03-16 14:27:00 +0100

    chore: remove packages/ingest (#983)

--- packages/ingest/src/generate-lesson-plans/user-prompt-parts/learningOutcome.promptPart.ts
+++
@@ -1,8 +0,0 @@
-import type { RawLesson } from "../../zod-schema/zodSchema";
-
-export const learningOutcomePromptPart = ({ pupilLessonOutcome }: RawLesson) =>
-  pupilLessonOutcome
-    ? `The lesson's learningOutcome should match the following text exactly:
-
-- ${pupilLessonOutcome}`
-    : null;
