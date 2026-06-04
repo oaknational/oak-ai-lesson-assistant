be63b8f1bb5c007b5c3284c91fa8bdb11bd81da8
Joe Baker <joe.baker@thenational.academy>
2026-06-04 15:36:05 +0100

    fix: lint errors

--- apps/nextjs/src/components/OakBoxWithCustomWidth.tsx
+++ apps/nextjs/src/components/OakBoxWithCustomWidth.tsx
@@ -2,7 +2,7 @@ import { OakBox } from "@oaknational/oak-components";
 import styled from "styled-components";
 
 const OakBoxWithCustomWidth = styled(OakBox)<{ width: number }>`
-  width: ${({ width }) => width}px;
+  width: ${({ width }: { width: number }) => width}px;
 `;
 
 export default OakBoxWithCustomWidth;
